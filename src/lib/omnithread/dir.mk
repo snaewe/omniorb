c0c0f4a03be0e17e6cee28e33a5b75d60e97f085
tjr <tjr>
1996-10-11 10:03:40 +0000

    Initial revision

---
+++ src/lib/omnithread/dir.mk
@@ -0,0 +1,28 @@
+ifeq ($(ThreadSystem),Solaris)
+CXXSRCS = solaris.cc
+OBJS = solaris.o
+DIR_CPPFLAGS = $(OMNITHREAD_CPPFLAGS)
+endif
+
+ifeq ($(ThreadSystem),Posix)
+CXXSRCS = posix.cc
+OBJS = posix.o
+DIR_CPPFLAGS = $(OMNITHREAD_CPPFLAGS) $(OMNITHREAD_POSIX_CPPFLAGS)
+endif
+
+ifeq ($(ThreadSystem),NT)
+CXXSRCS = nt.cc
+OBJS = nt.o
+DIR_CPPFLAGS = $(OMNITHREAD_CPPFLAGS)
+endif
+
+
+lib = $(patsubst %,$(LibPattern),omnithread)
+
+all:: $(lib)
+
+$(lib): $(OBJS)
+	@$(StaticLinkLibrary)
+
+export: $(lib)
+	@$(ExportLibrary)
