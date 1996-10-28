8c9b361234123c8669c7077a925d8e48eb5e11bc
tjr <tjr>
1996-10-28 19:07:24 +0000

    fix stupid bug

--- src/tool/win32/omake.c
+++ src/tool/win32/omake.c
@@ -146,7 +146,7 @@ int main(int argc, char **argv)
   for (i = 1; i < argc; i++) {
     makeCmd[nArgs++] = argv[i];
     if (!quiet) {
-      printf("'%s' ",makeCmd[i]);
+      printf("'%s' ",argv[i]);
     }
   }
 
