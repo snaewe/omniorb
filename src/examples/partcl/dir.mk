006b78849920966fc3d274d562dbb1e127334fdc
tjr <tjr>
1996-10-31 11:51:45 +0000

    don't need omnithread as well as omniParTcl

--- src/examples/partcl/dir.mk
+++ src/examples/partcl/dir.mk
@@ -1,6 +1,6 @@
 CXXSRCS = pttwish.cc
 
-DIR_CPPFLAGS = $(OMNITHREAD_CPPFLAGS) $(OMNIPARTCL_CPPFLAGS)
+DIR_CPPFLAGS = $(OMNIPARTCL_CPPFLAGS)
 
 script = $(patsubst %,$(TclScriptPattern),ptt)
 
@@ -8,9 +8,8 @@ wish = $(patsubst %,$(BinPattern),pttwish)
 
 all:: $(wish) $(script)
 
-$(wish): pttwish.o $(OMNITHREAD_LIB_DEPEND) $(OMNIPARTCL_LIB_DEPEND)
-	@(libs="$(OMNITHREAD_LIB) $(OMNIPARTCL_LIB)"; \
-	  $(CXXExecutable))
+$(wish): pttwish.o $(OMNIPARTCL_LIB_DEPEND)
+	@(libs="$(OMNIPARTCL_LIB)"; $(CXXExecutable))
 
 $(script): ptt.tcl
 	@(wish="$(wish)"; $(TclScriptExecutable))
