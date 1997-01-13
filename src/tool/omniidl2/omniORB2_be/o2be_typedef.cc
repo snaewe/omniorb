cfe72d63b1ca1f6ae84f2efeb5965aa83d9f3cdc
sll <sll>
1997-01-13 15:21:37 +0000

    Now called the produce_typedef_hdr() function of the real type to generate the typedef declarations. Fixed fieldMemberType_{u,f}qname() for object reference.

--- src/tool/omniidl2/omniORB2_be/o2be_typedef.cc
+++ src/tool/omniidl2/omniORB2_be/o2be_typedef.cc
@@ -10,6 +10,11 @@
 
 /*
   $Log$
+  Revision 1.2  1997/01/13 15:21:37  sll
+  Now called the produce_typedef_hdr() function of the real type to
+  generate the typedef declarations.
+  Fixed fieldMemberType_{u,f}qname() for object reference.
+
   Revision 1.1  1997/01/08 17:32:59  sll
   Initial revision
 
@@ -41,12 +46,26 @@ o2be_typedef::o2be_typedef(AST_Type *bt, UTL_ScopedName *n, UTL_StrList *p)
     {
     case AST_Decl::NT_interface:
       pd_fm_uqname = new char[strlen(OBJREF_MEMBER_TEMPLATE_NAME)+
-			     strlen("<>") + strlen(uqname()) + 1];
+			     strlen(uqname()) +
+			     strlen(uqname()) + strlen("_Helper") + 4];
       strcpy(pd_fm_uqname,OBJREF_MEMBER_TEMPLATE_NAME);
       strcat(pd_fm_uqname,"<");
       strcat(pd_fm_uqname,uqname());
+      strcat(pd_fm_uqname,",");
+      strcat(pd_fm_uqname,uqname());
+      strcat(pd_fm_uqname,"_Helper");
       strcat(pd_fm_uqname,">");
-      pd_fm_fqname = pd_fm_uqname;
+
+      pd_fm_fqname = new char[strlen(OBJREF_MEMBER_TEMPLATE_NAME)+
+			     strlen(fqname()) +
+			     strlen(fqname()) + strlen("_Helper") + 4];
+      strcpy(pd_fm_fqname,OBJREF_MEMBER_TEMPLATE_NAME);
+      strcat(pd_fm_fqname,"<");
+      strcat(pd_fm_fqname,fqname());
+      strcat(pd_fm_fqname,",");
+      strcat(pd_fm_fqname,fqname());
+      strcat(pd_fm_fqname,"_Helper");
+      strcat(pd_fm_fqname,">");
       break;
     case AST_Decl::NT_string:
       pd_fm_uqname = new char[strlen(STRING_MEMBER_NAME)+1];
@@ -73,51 +92,32 @@ o2be_typedef::produce_hdr(fstream &s)
   switch (decl->node_type())
     {
     case AST_Decl::NT_pre_defined:
-      if (o2be_predefined_type::narrow_from_decl(decl)->pt() ==
-	  AST_PredefinedType::PT_any) {
-	IND(s); s << "typedef " << tname << " " << uqname() << ";\n";
-	IND(s); s << "typedef " << tname << "_var " << uqname() << "_var;\n\n";
-      }
-      else {
-	IND(s); s << "typedef " << tname << " " << uqname() << ";\n\n";
-      }
+      o2be_predefined_type::narrow_from_decl(decl)->produce_typedef_hdr(s,this);
       break;
     case AST_Decl::NT_enum:
-      IND(s); s << "typedef " << tname << " " << uqname() << ";\n\n";
+      o2be_enum::narrow_from_decl(decl)->produce_typedef_hdr(s,this);
       break;
     case AST_Decl::NT_interface:
-      IND(s); s << "typedef " << tname << " " << uqname() << ";\n";
-      IND(s); s << "typedef " << tname << "_ptr " << uqname() << "_ptr;\n";
-      IND(s); s << "typedef " << tname << "Ref " << uqname() << "Ref;\n";
-      IND(s); s << "typedef " << tname << "_var " << uqname() << "_var;\n\n";
+      o2be_interface::narrow_from_decl(decl)->produce_typedef_hdr(s,this);
       break;
     case AST_Decl::NT_union:
-      IND(s); s << "typedef " << tname << " " << uqname() << ";\n";
-      IND(s); s << "typedef " << tname << "_var " << uqname() << "_var;\n\n";
+      o2be_union::narrow_from_decl(decl)->produce_typedef_hdr(s,this);
       break;
     case AST_Decl::NT_struct:
-      IND(s); s << "typedef " << tname << " " << uqname() << ";\n";
-      IND(s); s << "typedef " << tname << "_var " << uqname() << "_var;\n\n";
+      o2be_structure::narrow_from_decl(decl)->produce_typedef_hdr(s,this);
       break;
     case AST_Decl::NT_string:
-      IND(s); s << "typedef " << tname << " " << uqname() << ";\n";
-      IND(s); s << "typedef " << tname << "_var " << uqname() << "_var;\n\n";
+      o2be_string::produce_typedef_hdr(s,this);
       break;
     case AST_Decl::NT_sequence:
-#ifdef USE_SEQUENCE_TEMPLATE_IN_PLACE
-      IND(s); s << "typedef " 
-		<< o2be_sequence::narrow_from_decl(decl)->seq_template_name()
-		<< " " << uqname() << ";\n";
-      IND(s); s << "typedef " 
-		<< SEQUENCE_VAR_TEMPLATE
-		<< "<" << uqname() << "> " << uqname() << "_var;\n\n";
-#else
-      IND(s); s << "typedef " << tname << " " << uqname() << ";\n";
-      IND(s); s << "typedef " << tname << "_var " << uqname() << "_var;\n\n";
-#endif
+      o2be_sequence::narrow_from_decl(decl)->produce_typedef_hdr(s,this);
       break;
     case AST_Decl::NT_array:
-      o2be_array::narrow_from_decl(decl)->produce_typedef_hdr(s,this);
+      if (base_type()->node_type() == AST_Decl::NT_array)
+	o2be_array::narrow_from_decl(decl)->produce_hdr(s,this);
+      else
+	o2be_array::produce_typedef_hdr(s,this,
+					o2be_typedef::narrow_from_decl(base_type()));
       break;
     default:
       throw o2be_internal_error(__FILE__,__LINE__,"Unexpected argument type");
@@ -131,7 +131,7 @@ o2be_typedef::produce_skel(fstream &s)
   AST_Decl *decl = base_type();
 
   if (decl->node_type() == AST_Decl::NT_array)
-    o2be_array::narrow_from_decl(decl)->produce_typedef_skel(s,this);
+    o2be_array::narrow_from_decl(decl)->produce_skel(s,this);
 }
 
 IMPL_NARROW_METHODS1(o2be_typedef, AST_Typedef)
