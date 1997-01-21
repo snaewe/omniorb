fa979cbf2c3c896067cd638640ad01c9bd8eae48
ewc <ewc>
1997-01-21 14:23:59 +0000

    Modified to (IDL) compile the COSS Naming Service interfaces and to compile additional files added to support the initial reference interface.

--- src/lib/omniORB2/orbcore/dir.mk
+++ src/lib/omniORB2/orbcore/dir.mk
@@ -1,3 +1,8 @@
+# dir.mk for omniORB2.
+#
+#
+
+
 ifdef UnixArchitecture
 NETLIBSRCS = tcpSocket_UNIX.cc
 NETLIBOBJS = tcpSocket_UNIX.o
@@ -10,16 +15,21 @@ NETLIBOBJS = tcpSocket_ATMos.o
 DIR_CPPFLAGS = -DATMosArchitecture
 endif
 
+# Required to build Naming.hh and NamingSK.cc:
+CorbaImplementation = OMNIORB2
+vpath %.idl $(VPATH)
+CORBA_STUB_HDRS = Naming.hh
+
 ORB2_SRCS = constants.cc corbaBoa.cc corbaObject.cc corbaOrb.cc \
             corbaString.cc \
-          exception.cc giopClient.cc giopServer.cc ior.cc \
-          libcWrapper.cc mbufferedStream.cc nbufferedStream.cc \
+          exception.cc giopClient.cc giopServer.cc initFile.cc ior.cc \
+          libcWrapper.cc mbufferedStream.cc NamingSK.cc nbufferedStream.cc \
           object.cc objectRef.cc orb.cc strand.cc $(NETLIBSRCS)
 
 ORB2_OBJS = constants.o corbaBoa.o corbaObject.o corbaOrb.o \
             corbaString.o \
-            exception.o giopClient.o giopServer.o ior.o \
-            libcWrapper.o mbufferedStream.o nbufferedStream.o \
+            exception.o giopClient.o giopServer.o initFile.o ior.o \
+            libcWrapper.o mbufferedStream.o NamingSK.o nbufferedStream.o \
             object.o objectRef.o orb.o strand.o $(NETLIBOBJS)
 
 DIR_CPPFLAGS += $(OMNITHREAD_CPPFLAGS)
@@ -47,8 +57,14 @@ all:: $(lib)
 $(lib): $(ORB2_OBJS)
 	@$(StaticLinkLibrary)
 
+Naming.hh:	Naming.idl
+	$(OMNIORB2_IDL) $^
+
 clean::
 	$(RM) $(lib)
 
 export:: $(lib)
 	@$(ExportLibrary)
+
+export:: Naming.hh
+	@(file="Naming.hh"; dir="$(EXPORT_TREE)/$(INCDIR)"; $(ExportFileToDir))
