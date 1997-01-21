19c91d593490276c6226d3c90421aa059e4f38dd
ewc <ewc>
1997-01-21 14:19:44 +0000

    Added support for initial references interface.

--- src/lib/omniORB/orbcore/corbaOrb.cc
+++ src/lib/omniORB/orbcore/corbaOrb.cc
@@ -11,9 +11,12 @@
 
 /*
   $Log$
-  Revision 1.1  1997/01/08 17:26:01  sll
-  Initial revision
+  Revision 1.2  1997/01/21 14:19:44  ewc
+  Added support for initial references interface.
 
+// Revision 1.1  1997/01/08  17:26:01  sll
+// Initial revision
+//
  */
 
 #include <omniORB2/CORBA.h>
@@ -109,3 +112,81 @@ CORBA::is_nil(ORB_ptr p)
   return (p == 0) ? 1 : 0;
 }
 
+
+
+// Implementation of Initial Reference member functions:
+
+
+CORBA::Object_ptr 
+CORBA::ORB::resolve_initial_references(const char* identifier)
+{
+  omniObject* objptr = omniORB::resolveInitRef(identifier);
+
+  if (objptr)
+      return (CORBA::Object_ptr) (objptr->_widenFromTheMostDerivedIntf(0));
+  else return CORBA::Object::_nil();
+}
+
+
+CORBA::
+ORB::ObjectIdList* CORBA::ORB::list_initial_services()
+{
+char** servicelist;
+
+unsigned long listlen = omniORB::listInitServices(servicelist);
+
+ObjectIdList* idlist;
+
+if (listlen == 0) idlist = new ObjectIdList(0,0,NULL);
+else idlist = 
+      new ObjectIdList(listlen,listlen,(CORBA::String_member*) servicelist,1);
+
+return idlist;
+}
+
+
+// Implementation of InvalidName member functions:
+// [ InvalidName = exception raised by resolve_initial_references() ]
+
+CORBA::
+ORB::InvalidName::InvalidName(const ORB::InvalidName &_s)
+{
+}
+
+CORBA::
+ORB::InvalidName & CORBA::ORB::InvalidName::operator=(const ORB::InvalidName &_s)
+{
+  return *this;
+}
+
+size_t
+CORBA::
+ORB::InvalidName::NP_alignedSize(size_t _initialoffset)
+{
+  size_t _msgsize = _initialoffset;
+  return _msgsize;
+}
+
+void
+CORBA::
+ORB::InvalidName::operator>>= (NetBufferedStream &_n)
+{
+}
+
+void
+CORBA::
+ORB::InvalidName::operator<<= (NetBufferedStream &_n)
+{
+}
+
+void
+CORBA::
+ORB::InvalidName::operator>>= (MemBufferedStream &_n)
+{
+}
+
+void
+CORBA::
+ORB::InvalidName::operator<<= (MemBufferedStream &_n)
+{
+}
