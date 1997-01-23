d98a2f7fb19ff46c6d50cc00c22c46b5b657dc25
sll <sll>
1997-01-23 18:30:17 +0000

    Added back the missing NamingSK.o.

--- src/lib/omniORB2/dynamic/dir.mk
+++ src/lib/omniORB2/dynamic/dir.mk
@@ -28,13 +28,13 @@ UNSHARED_OBJS = unshared.o
 ORB2_SRCS = constants.cc corbaBoa.cc corbaObject.cc corbaOrb.cc \
             corbaString.cc \
           exception.cc giopClient.cc giopServer.cc initFile.cc ior.cc \
-          libcWrapper.cc mbufferedStream.cc nbufferedStream.cc \
+          libcWrapper.cc mbufferedStream.cc nbufferedStream.cc NamingSK.cc \
           object.cc objectRef.cc orb.cc strand.cc $(NETLIBSRCS)
 
 ORB2_OBJS = constants.o corbaBoa.o corbaObject.o corbaOrb.o \
             corbaString.o \
             exception.o giopClient.o giopServer.o initFile.o ior.o \
-            libcWrapper.o mbufferedStream.o nbufferedStream.o \
+            libcWrapper.o mbufferedStream.o nbufferedStream.o NamingSK.o \
             object.o objectRef.o orb.o strand.o $(NETLIBOBJS)
 
 DIR_CPPFLAGS += $(OMNITHREAD_CPPFLAGS)
