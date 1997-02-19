35262fe0b6c40e15598ef364300df21afb378d70
ewc <ewc>
1997-02-19 10:54:16 +0000

    Added support for Windows NT

--- src/lib/omniORB2/orbcore/dir.mk
+++ src/lib/omniORB2/orbcore/dir.mk
@@ -3,12 +3,31 @@
 # Build a static library in this directory and a shared library in ./sharedlib
 
 
+#Shared library only available on Unix at the moment:
+ifdef UnixArchitecture
 SUBDIRS = sharedlib
+endif
 
 ifdef UnixArchitecture
+# Default location of the omniORB2 configuration file [falls back to this if
+# the environment variable OMNIORB_CONFIG is not set] :
+CONFIG_DEFAULT_LOCATION = \"/project/omni/var/omniORB.cfg\"
 NETLIBSRCS = tcpSocket_UNIX.cc
 NETLIBOBJS = tcpSocket_UNIX.o
-DIR_CPPFLAGS = -DUnixArchitecture
+DIR_CPPFLAGS = -DUnixArchitecture 
+CXXDEBUGFLAGS = -fast
+DIR_CPPFLAGS += -DCONFIG_DEFAULT_LOCATION=$(CONFIG_DEFAULT_LOCATION)
+endif
+
+ifdef NTArchitecture
+# Default location of the omniORB2 configuration file [falls back to this if
+# the environment variable OMNIORB_CONFIG is not set] :
+CONFIG_DEFAULT_LOCATION = \"E:\\corba2\\test\"
+NETLIBSRCS = tcpSocket_NT.cc
+NETLIBOBJS = tcpSocket_NT.o
+DIR_CPPFLAGS = -MD -W3 -GX -O2 -D "NDEBUG" -D "WIN32" 
+DIR_CPPFLAGS += -D "_WINDOWS" -D "__NT__" -D "_X86_" -D "NTArchitecture"
+DIR_CPPFLAGS += -I"C:\MSDEV\INCLUDE" 
 endif
 
 ifdef ATMosArchitecture
@@ -18,9 +37,16 @@ DIR_CPPFLAGS = -DATMosArchitecture
 endif
 
 # Required to build Naming.hh and NamingSK.cc:
+ifndef NTArchitecture
 CorbaImplementation = OMNIORB2
 vpath %.idl $(VPATH)
 CORBA_STUB_HDRS = Naming.hh
+NAMINGSRC = NamingSK.cc
+NAMINGOBJ = NamingSK.o
+else 
+NAMINGSRC = NamingSK_NT.cc
+NAMINGOBJ = NamingSK_NT.o
+endif
 
 UNSHARED_SRCS = unshared.cc
 UNSHARED_OBJS = unshared.o
@@ -28,13 +54,13 @@ UNSHARED_OBJS = unshared.o
 ORB2_SRCS = constants.cc corbaBoa.cc corbaObject.cc corbaOrb.cc \
             corbaString.cc \
           exception.cc giopClient.cc giopServer.cc initFile.cc ior.cc \
-          libcWrapper.cc mbufferedStream.cc nbufferedStream.cc NamingSK.cc \
+          libcWrapper.cc mbufferedStream.cc nbufferedStream.cc $(NAMINGSRC) \
           object.cc objectRef.cc orb.cc strand.cc $(NETLIBSRCS)
 
 ORB2_OBJS = constants.o corbaBoa.o corbaObject.o corbaOrb.o \
             corbaString.o \
             exception.o giopClient.o giopServer.o initFile.o ior.o \
-            libcWrapper.o mbufferedStream.o nbufferedStream.o NamingSK.o \
+            libcWrapper.o mbufferedStream.o nbufferedStream.o $(NAMINGOBJ) \
             object.o objectRef.o orb.o strand.o $(NETLIBOBJS)
 
 DIR_CPPFLAGS += $(OMNITHREAD_CPPFLAGS)
@@ -43,12 +69,12 @@ DIR_CPPFLAGS += -I./..
 ifeq ($(CXX),g++)
 CXXDEBUGFLAGS = -g
 DIR_CPPFLAGS += -D__OMNIORB__ -fhandle-exceptions -Wall -Wno-unused 
-else
-CXXDEBUGFLAGS = -g
+else 
 DIR_CPPFLAGS += -D__OMNIORB__
 endif
 
 ifeq ($(platform),arm_atmos_4.0/atb)
+CXXDEBUGFLAGS = -g
 DIR_CPPFLAGS +=  -I/project/atmos/release4.0/atb/ip \
 	-D__cplusplus -fhandle-exceptions -Wall -Wno-unused
 endif
@@ -75,8 +101,10 @@ clean::
 export:: $(lib)
 	@$(ExportLibrary)
 
+ifndef NTArchitecture
 export:: Naming.hh
 	@(file="Naming.hh"; dir="$(EXPORT_TREE)/$(INCDIR)/omniORB2"; $(ExportFileToDir))
+endif
 
 export::
 	@$(MakeSubdirs)
