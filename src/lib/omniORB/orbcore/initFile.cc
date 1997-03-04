be427d96acfe6ace6264eaa325c20815c2a2a546
ewc <ewc>
1997-03-04 10:31:15 +0000

    Added support for obtaining initial object references from Windows NT/'95 registry

--- src/lib/omniORB/orbcore/initFile.cc
+++ src/lib/omniORB/orbcore/initFile.cc
@@ -11,8 +11,9 @@
 
 /*
   $Log$
-  Revision 1.4  1997/02/19 11:01:59  ewc
-  Added support for NT
+  Revision 1.5  1997/03/04 10:31:15  ewc
+  Added support for obtaining initial object references from Windows NT/'95
+  registry
 
 // Revision 1.3  1997/01/22  14:33:51  ewc
 // Small bug fix to file read.
@@ -33,21 +34,35 @@
 #include <kernel.h>
 #endif
 
-#ifdef  __NT__
-
-#ifndef CONFIG_DEFAULT_LOCATION
-// MSVC++ 4.2 through ODE doesn't like specifying this on command line:
-#define CONFIG_DEFAULT_LOCATION  "\\\\shallot\\omni\\var\\omniorb.cfg"
-#endif
-
+#ifdef __NT__
+#include <windows.h>
+#include <winreg.h>
 #endif
 
 #include <omniORB2/CORBA.h>
 
 
+#ifndef INIT_ENV_VAR
+#define INIT_ENV_VAR "OMNIORB_CONFIG"    
+                      // Environment variable that points to configuration file. 
+#endif
+
+#ifndef INIT_REGKEY
+#define INIT_REGKEY  "SOFTWARE\\ORL\\omniORB\\2.0"
+                     // Path to Registry Keys for omniORB2 [NT specific]
+#endif
+
+
+#define INIT_MAX_CONFIG 10   
+                     // Maximum number of entries in configuration 
+
 initFile::initFile()
 {
 NameService = NULL;
+
+#ifdef __NT__
+use_registry = 0;
+#endif
 }
 
 
@@ -67,17 +82,44 @@ char* config_fname;
 
 #if defined(UnixArchitecture) || defined(NTArchitecture)
 
-// XXXXXX
-// Eventually, use system registry for Windows NT
-
 char* tmp_fname;
 
-if ((tmp_fname = getenv("OMNIORB_CONFIG")) == NULL)
+if ((tmp_fname = getenv(INIT_ENV_VAR)) == NULL)
   {
 #if defined(UnixArchitecture)
     config_fname = strdup(CONFIG_DEFAULT_LOCATION);
 #elif defined(NTArchitecture)
-    config_fname = strdup(CONFIG_DEFAULT_LOCATION);
+
+    // Can't find configuration file using the
+    // environment variable -- NT will attempt to use the registry to find
+    // configuration information.
+
+    // Open the registry, and get a handle to the key:
+
+    char* omniORBSubKey = INIT_REGKEY;
+    if (RegOpenKeyEx(HKEY_LOCAL_MACHINE,omniORBSubKey,0,KEY_QUERY_VALUE,
+		        &init_hkey) != ERROR_SUCCESS)
+      {
+       	// Don't throw exception, as information in file 
+	// may not be used - fail only when a call that requires
+	// the information is made.
+
+	return;
+      }
+    else 
+      {
+	DWORD numVals;
+	RegQueryInfoKey(init_hkey,NULL,NULL,NULL,NULL,NULL,NULL,
+		        &numVals,&init_maxValLen,&init_maxDataLen,NULL,NULL);
+
+	if(numVals == 0) 
+	  {
+	    // No values found:
+	    noValsFound();
+	  }
+
+        use_registry = 1;
+      }
 #endif
   }
 else
@@ -91,43 +133,30 @@ config_fname = strdup("//isfs/omniORB.cfg");
 
 #endif
 
-
-// Read file:
-
-FILE* iFile;
-if ((iFile = fopen(config_fname,"r")) == NULL)
+#ifdef __NT__
+if (!use_registry) 
   {
-    // Can't open file.
-    // Don't throw exception, as information in file 
-    // may not be used - fail only when a call that requires
-    // the information is made.
+#endif
+    if (read_file(config_fname) < 0)
+      {
+	// Can't open file.
+	// Don't throw exception, as information in file 
+	// may not be used - fail only when a call that requires
+	// the information is made.
 
+	free(config_fname);
+	return;
+      }
     free(config_fname);
-    return;
+#ifdef __NT__
   }
-
-free(config_fname);
-
-fsize = 0;
-fseek(iFile,0L,SEEK_END);
-fsize = ftell(iFile);
-rewind(iFile);
-
-fData = new char[fsize+1];
-if (fData == NULL)
-  {
-    throw CORBA::NO_MEMORY(0,CORBA::COMPLETED_NO);
-  }
-
-fread((void*) fData,1,fsize,iFile);
-fclose(iFile);
-
+#endif
 
 // Get configuration data:
 
 char* entryname;
 char* data;
-int multcheck[MAX_CONFIG] = { 0 }; 
+int multcheck[INIT_MAX_CONFIG] = { 0 }; 
 
 while(getnextentry(entryname,data))
   {
@@ -165,10 +194,10 @@ while(getnextentry(entryname,data))
 
 #ifndef __atmos__
 	cerr << "Configuration error:  Unknown field (" << entryname << ") "
-	     << " found in configuration file." << endl;
+	     << " found in configuration." << endl;
 #else
 	kprintf("Configuration error:  ");
-	kprintf("Unknown field (%s) found in configutation file.\n",entryname);
+	kprintf("Unknown field (%s) found in configuration file.\n",entryname);
 #endif
 	throw CORBA::INITIALIZE(0,CORBA::COMPLETED_NO);
       }
@@ -182,9 +211,37 @@ return;
 }
 
 
+int initFile::read_file(char* config_fname)
+{
+// Read the configuration file:
+
+FILE* iFile;
+if ((iFile = fopen(config_fname,"r")) == NULL) return -1;
+
+fsize = 0;
+fseek(iFile,0L,SEEK_END);
+fsize = ftell(iFile);
+rewind(iFile);
+
+fData = new char[fsize+1];
+if (fData == NULL) throw CORBA::NO_MEMORY(0,CORBA::COMPLETED_NO);
+
+fread((void*) fData,1,fsize,iFile);
+fclose(iFile);
+return 0;
+}
+
+
+
 int initFile::getnextentry(char*& entryname, char*& data)
 {
   // Get next entry in config file, and associated data
+
+#ifdef __NT__
+  // Use registry for NT if environment variable for config file not set:
+if (use_registry)
+ return getRegistryEntry(entryname, data);
+#endif
 
 static long currpos = 0;
 
@@ -253,13 +310,49 @@ return 1;
 }
 
 
+
+#ifdef __NT__
+
+// NT member function to use registry:
+
+int initFile::getRegistryEntry(char*& entryname, char*& data)
+{
+static DWORD curr_index = 0;
+
+DWORD dataType;
+DWORD init_ValLen = init_maxValLen+1;
+DWORD init_DataLen = init_maxDataLen+1;
+entryname = new char[init_ValLen];
+data = new char[init_DataLen];
+
+if (RegEnumValue(init_hkey,curr_index++,(LPTSTR) entryname,&init_ValLen,NULL,
+		    &dataType,(LPBYTE) data, &init_DataLen) != ERROR_SUCCESS)
+  {
+    delete[] entryname;
+    delete[] data;
+    return 0;
+  }
+
+if (dataType != REG_SZ)
+  {
+    // Data is not a character string.
+    formaterr(entryname);
+  }
+    
+return 1;
+}
+
+#endif
+
+
+
 // Implementations of inline error-report functions:
 
 void initFile::multerr(char* entryname)
 {
 #ifndef __atmos__
 cerr << "Configuration error: Multiple entries found for field "
-     << entryname << " in configuration file." << endl;
+     << entryname << " ." << endl;
 #else
 kprintf("Configuration error: ");
 kprintf("Multiple entries found for field %s in configuration file.\n",
@@ -309,3 +402,26 @@ kprintf("Configuration error: Invalid object reference supplied for %s.\n",
 throw CORBA::INITIALIZE(0,CORBA::COMPLETED_NO);
 }
 
+
+#ifdef __NT__
+
+// NT-specific error reporting functions:
+
+void initFile::noValsFound()
+{
+  cerr << "Configuration error: No values found in registry key " 
+       << INIT_REGKEY << endl;
+
+  throw CORBA::INITIALIZE(0,CORBA::COMPLETED_NO);
+}
+
+
+void initFile::formaterr(char* entryname)
+{
+  cerr << "Configuration error: Data for value " << entryname 
+       << " is not a character string." << endl;
+
+  throw CORBA::INITIALIZE(0,CORBA::COMPLETED_NO);
+}
+
+#endif
