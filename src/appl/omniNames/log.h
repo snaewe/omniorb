d3c537900c2b9110280346035fd0099ec7c33321
tjr <tjr>
1997-03-20 10:21:20 +0000

    improve error handling

--- src/appl/omniNames/log.h
+++ src/appl/omniNames/log.h
@@ -15,17 +15,15 @@ class log {
 
   int port;
 
-  int startingUp;
+  int startingUp;	// true while reading log file initially.
+  int firstTime;	// true if -start option was given.
+  int checkpointNeeded;	// true if changes have been made since last checkpoint
 
-  int firstTime;
+  int line;		// current line number when reading log file initially.
 
   //
   // functions to write to a file
   //
-
-  void putString(const char* str, ostream& file);
-
-  void putKey(const omniORB::objectKey& key, ostream& file);
 
   void putPort(int port, ostream& file);
 
@@ -40,29 +38,40 @@ class log {
   void putUnbind(CosNaming::NamingContext_ptr nc, const CosNaming::Name& n,
 		 ostream& file);
 
+  void putKey(const omniORB::objectKey& key, ostream& file);
+
+  void putString(const char* str, ostream& file);
+
   //
   // functions to read from a file
   //
 
+  void getPort(istream& file);
+
+  void getCreate(istream& file);
+
+  void getDestroy(istream& file);
+
+  void getBind(istream& file);
+
+  void getUnbind(istream& file);
+
+  void getKey(omniORB::objectKey& k, istream& file);
+
+  void getFinalString(char*& buf, istream& file);
+
+  void getNonfinalString(char*& buf, istream& file);
+
   int getString(char*& buf, istream& file);
-
-  int getKey(omniORB::objectKey& k, istream& file);
-
-  int getCreate(istream& file);
-
-  int getDestroy(istream& file);
-
-  int getBind(istream& file);
-
-  int getUnbind(istream& file);
 
 public:
 
   class IOError {};
+  class ParseError {};
 
   log(int& port);
 
-  void redo(CORBA::ORB_ptr o, CORBA::BOA_ptr b);
+  void init(CORBA::ORB_ptr o, CORBA::BOA_ptr b);
 
   void create(const omniORB::objectKey& key);
   void destroy(CosNaming::NamingContext_ptr nc);
