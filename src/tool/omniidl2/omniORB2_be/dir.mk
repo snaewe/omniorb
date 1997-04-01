1dc67f9cca0da08a540eede5e4524d44e03fe6f1
ewc <ewc>
1997-04-01 16:55:38 +0000

    Added support for Win32

--- src/tool/omniidl2/omniORB2_be/dir.mk
+++ src/tool/omniidl2/omniORB2_be/dir.mk
@@ -44,8 +44,12 @@ OBJS         = \
 
 DIR_CPPFLAGS = -I../include $(patsubst %,-I%/../include,$(VPATH))
 
-ifeq ($(CXX),g++)
-CXXDEBUGFLAGS += -fhandle-exceptions
+ifdef NTArchitecture
+# Note the use of -W0 in CXXOPTIONS - this is used to stop pragma warning 
+# messages - should replace with a warning(disable,4068) pragma in the source
+# code
+DIR_CPPFLAGS += -D "NDEBUG" -D "_WINDOWS" -D "_X86_" -D "NTArchitecture" 
+CXXOPTIONS = -ML -W0 -GX -O2 
 endif
 
 lib = $(patsubst %,$(LibPattern),omniORB2_be)
