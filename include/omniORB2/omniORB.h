c7931dde79246cc988b9e11948350fa4cb4e96f9
sll <sll>
1997-04-23 13:56:41 +0000

    Added strictIIOP flag.

--- include/omniORB2/omniORB.h
+++ include/omniORB2/omniORB.h
@@ -11,9 +11,12 @@
 
 /*
   $Log$
-  Revision 1.3  1997/03/26 17:38:42  ewc
-  Runtime converted to Win32 DLL
+  Revision 1.4  1997/04/23 13:56:41  sll
+  Added strictIIOP flag.
 
+ * Revision 1.3  1997/03/26  17:38:42  ewc
+ * Runtime converted to Win32 DLL
+ *
  * Revision 1.2  1997/03/11  19:56:43  sll
  * keyToOctetSequence and octetSequenceToKey should be static. Fixed.
  *
@@ -25,8 +28,8 @@
 #ifndef __OMNIORB_H__
 #define __OMNIORB_H__
 
+class _OMNIORB2_NTDLL_ omniORB {
 
-class  _OMNIORB2_NTDLL_ omniORB {
 public:
 
   ///////////////////////////////////////////////////////////////////////
@@ -44,6 +47,35 @@ public:
   //     option: -ORBtraceLevel <n>, or by direct assignment to this   //
   //     variable.                                                     //
   ///////////////////////////////////////////////////////////////////////
+
+  ///////////////////////////////////////////////////////////////////////
+  // strictIIOP flag                                                   //
+  //   Enable vigorous check on incoming IIOP messages                 //
+  //                                                                   //
+  // In some (sloppy) IIOP implementation, the message size value in   //
+  // the header can be larger than the actual body size, i.e. there is //
+  // garbage at the end. As the spec does not say the message size     //
+  // must match the body size exactly, this is not a clear violation   //
+  // of the spec.                                                      //
+  //                                                                   //
+  // If this flag is non-zero, the incoming message is expected to     //
+  // be well behaved. Any messages that have garbage at the end will   //
+  // be rejected.
+  //
+  // The default value of this flag is zero and the ORB would silently //
+  // skip the unread part. The problem with this behaviour is that the //
+  // header message size may actually be garbage value, caused by a bug//
+  // in the sender's code. Th receiving thread may forever block on    //
+  // the strand as it tries to read more data from it. In this case the//
+  // sender won't send anymore as it thinks it has marshalled in all   //
+  // the data.							       //
+  static _CORBA_Boolean   strictIIOP;                                  //
+  //                                                                   //
+  //     This value can be changed at runtime either by command-line   //
+  //     option: -ORBstrictIIOP <0|1>, or by direct assignment to this //
+  //     variable.                                                     //
+  ///////////////////////////////////////////////////////////////////////
+
 
   ////////////////////////////////////////////////////////////////////////
   // objectKey is a data type that uniquely identify each object        //
