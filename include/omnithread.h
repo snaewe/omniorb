bd26a21a3f789ff3a2d923fc554e762f14731026
sll <sll>
1997-05-06 15:52:32 +0000

    Public release.

--- include/omnithread.h
+++ include/omnithread.h
@@ -1,7 +1,26 @@
 //				Package : omnithread
 // omnithread.h			Created : 7/94 tjr
 //
-// Copyright (C) Olivetti Research Ltd 1994, 1995
+//    Copyright (C) 1994,1995,1996, 1997 Olivetti & Oracle Research Laboratory
+//
+//    This file is part of the omnithread library
+//
+//    The omnithread library is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Library General Public
+//    License as published by the Free Software Foundation; either
+//    version 2 of the License, or (at your option) any later version.
+//
+//    This library is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Library General Public License for more details.
+//
+//    You should have received a copy of the GNU Library General Public
+//    License along with this library; if not, write to the Free
+//    Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  
+//    02111-1307, USA
+//
+
 //
 // Interface to OMNI thread abstraction.
 //
@@ -15,19 +34,6 @@
 // provided, the intention here has been to be as POSIX-like as possible.  This
 // is why there is no semaphore timed wait, for example.
 //
-
-/* 
- * $Log$
- * Revision 1.4  1997/05/06 15:47:50  ewc
- * Fixed small Macro bug.
- *
- * Revision 1.3  1997/04/01  12:28:50  ewc
- * *** empty log message ***
- *
- * Revision 1.2  1995/08/17  10:14:55  tjr
- * new version of OMNI thread abstraction
- *
- */
 
 #ifndef __omnithread_h_
 #define __omnithread_h_
