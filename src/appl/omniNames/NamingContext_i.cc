080b67ba15595eb41f3450fbaf164e830ce29561
tjr <tjr>
1997-05-07 14:36:14 +0000

    *** empty log message ***

--- src/appl/omniNames/NamingContext_i.cc
+++ src/appl/omniNames/NamingContext_i.cc
@@ -1,8 +1,32 @@
+// -*- Mode: C++; -*-
+//                          Package   : omniNames
+// NamingContext_i.cc       Author    : Tristan Richardson (tjr)
+//
+//    Copyright (C) 1997 Olivetti & Oracle Research Laboratory
+//
+//  This file is part of omniNames.
+//
+//  omniNames is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,
+//  USA.
+//
+
 #include <iostream.h>
 #include <assert.h>
-#include "NamingContext_i.h"
-#include "ObjectBinding.h"
-#include "BindingIterator_i.h"
+#include <NamingContext_i.h>
+#include <ObjectBinding.h>
+#include <BindingIterator_i.h>
 
 #ifdef DEBUG_NC
 #define DB(x) x
