7566bdf0fb5dca7d013722212e0e6c7035c7c24b
sll <sll>
1997-08-21 22:21:38 +0000

    New extern function _CORBA_use_nil_ptr_as_nil_objref().

--- include/omniORB2/CORBA_basetypes.h
+++ include/omniORB2/CORBA_basetypes.h
@@ -28,9 +28,12 @@
 
 /*
  $Log$
- Revision 1.3  1997/05/06 16:05:20  sll
- Public release.
+ Revision 1.4  1997/08/21 22:21:38  sll
+ New extern function _CORBA_use_nil_ptr_as_nil_objref().
 
+ * Revision 1.3  1997/05/06  16:05:20  sll
+ * Public release.
+ *
  */
 
 #ifndef __CORBA_BASETYPES_H__
@@ -71,5 +74,6 @@ typedef double                    _CORBA_Double;
 extern void _CORBA_new_operator_return_null();
 extern void _CORBA_bound_check_error();
 extern void _CORBA_marshal_error();
+extern _CORBA_Boolean _CORBA_use_nil_ptr_as_nil_objref();
 
 #endif // __CORBA_BASETYPES_H__
