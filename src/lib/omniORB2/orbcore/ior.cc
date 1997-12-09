130c2468d0eeec0165b84a30895dcf267d469897
sll <sll>
1997-12-09 17:32:39 +0000

    Removed obsoluted functions IIOP::profileToEncapStream and IIOP::EncapStreamToProfile. IOP::EncapStrToIor now accepts dodgy nil object reference.

--- src/lib/omniORB2/orbcore/ior.cc
+++ src/lib/omniORB2/orbcore/ior.cc
@@ -29,6 +29,11 @@
  
 /*
   $Log$
+  Revision 1.6  1997/12/09 17:32:39  sll
+  Removed obsoluted functions IIOP::profileToEncapStream and
+  IIOP::EncapStreamToProfile.
+  IOP::EncapStrToIor now accepts dodgy nil object reference.
+
   Revision 1.5  1997/08/21 22:04:23  sll
   minor cleanup to get rid of purify's warnings.
 
@@ -53,148 +58,6 @@
 #else
 #error "Swap32 has already been defined"
 #endif
-
-void
-IIOP::profileToEncapStream(IIOP::ProfileBody &p,
-			   _CORBA_Unbounded_Sequence_Octet &s)
-{
-  CORBA::ULong hlen = strlen((const char *)p.host) + 1;
-  CORBA::ULong klen = p.object_key.length();
-  {
-    // calculate the total size of the encapsulated stream
-    CORBA::ULong total = 8 + hlen;        // first 4 bytes + aligned host
-    total = ((total + 1) & ~(1)) + 2;     // aligned port value
-    total = ((total + 3) & ~(3)) + 4 +	// aligned object key
-      klen;
-    
-    s.length(total);
-  }
-
-
-  s[0] = omni::myByteOrder;
-  s[1] = IIOP::current_major;
-  s[2] = IIOP::current_minor;
-  s[3] = 0;
-  {
-    CORBA::ULong &l = (CORBA::ULong &) s[4];
-    l = hlen;
-  }
-  memcpy((void *)&(s[8]),(void *)p.host,hlen);
-  CORBA::ULong idx = ((8 + hlen) + 1) & ~(1);
-  {
-    CORBA::UShort &l = (CORBA::UShort &) s[idx];
-    l = p.port;
-  }
-  idx = ((idx + 2) + 3) & ~(3);
-  {
-    CORBA::ULong &l = (CORBA::ULong &) s [idx];
-    l = klen;
-  }
-  idx += 4;
-  memcpy((void *)&s[idx],(void *)&p.object_key[0],klen);
-  return;
-}
-
-void
-IIOP::EncapStreamToProfile(const _CORBA_Unbounded_Sequence_Octet &s,
-			   IIOP::ProfileBody &p)
-{
-  CORBA::ULong begin = 0;
-  CORBA::ULong end = 0;
-
-  // s[0] - byteorder
-  end += 1;
-  if (s.length() <= end)
-    throw CORBA::MARSHAL(0,CORBA::COMPLETED_NO);
-
-  CORBA::Boolean byteswap = ((s[begin] == omni::myByteOrder) ? 0 : 1);
-
-  // s[1] - iiop_version.major
-  // s[2] - iiop_version.minor
-  begin = end;
-  end = begin + 2;
-  if (s.length() <= end)
-    throw CORBA::MARSHAL(0,CORBA::COMPLETED_NO);
-  p.iiop_version.major = s[begin];
-  p.iiop_version.minor = s[begin+1];
-  if (p.iiop_version.major != IIOP::current_major ||
-      p.iiop_version.minor != IIOP::current_minor)
-    throw CORBA::MARSHAL(0,CORBA::COMPLETED_NO);
-
-  // s[3] - padding
-  // s[4] - s[7] host string length
-  begin = end + 1;
-  end = begin + 4;
-  if (s.length() <= end)
-    throw CORBA::MARSHAL(0,CORBA::COMPLETED_NO);
-  {
-    CORBA::ULong len;
-    if (!byteswap) {
-      len = ((CORBA::ULong &) s[begin]);
-    }
-    else {
-      CORBA::ULong t = ((CORBA::ULong &) s[begin]);
-      len = Swap32(t);
-    }
-
-    // s[8] - s[8+len-1] host string
-    begin = end;
-    end = begin + len;
-    if (s.length() <= end)
-      throw CORBA::MARSHAL(0,CORBA::COMPLETED_NO);
-
-    // Is this string null terminated?
-    if (((char)s[end-1]) != '\0')
-      throw CORBA::MARSHAL(0,CORBA::COMPLETED_NO);
-
-    p.host = new CORBA::Char[len];
-    if (!p.host)
-      throw CORBA::NO_MEMORY(0,CORBA::COMPLETED_NO);
-    memcpy((void *)p.host,(void *)&(s[begin]),len);
-  }
-    
-  // align to CORBA::UShort
-  begin = (end + 1) & ~(1);
-  // s[begin] port number
-  end = begin + 2;
-  if (s.length() <= end)
-    throw CORBA::MARSHAL(0,CORBA::COMPLETED_NO);
-  if (!byteswap) {
-    p.port = ((CORBA::UShort &) s[begin]);
-  }
-  else {
-    CORBA::UShort t = ((CORBA::UShort &) s[begin]);
-    p.port = Swap16(t);
-  }
-
-  // align to CORBA::ULong
-  begin = (end + 3) & ~(3);
-  // s[begin]  object key length
-  end = begin + 4;
-  if (s.length() <= end)
-    throw CORBA::MARSHAL(0,CORBA::COMPLETED_NO);
-  {
-    CORBA::ULong len;
-    if (!byteswap) {
-      len = ((CORBA::ULong &) s[begin]);
-    }
-    else {
-      CORBA::ULong t = ((CORBA::ULong &) s[begin]);
-      len = Swap32(t);
-    }
-
-    begin = end;
-    end = begin + len;
-    if (s.length() < end)
-      throw CORBA::MARSHAL(0,CORBA::COMPLETED_NO);
-
-    // extract object key
-    p.object_key.length(len);
-    memcpy((void *)&p.object_key[0],(void *)&(s[begin]),len);
-  }
-  return;
-}
-
 
 
 CORBA::Char * 
@@ -301,10 +164,41 @@ IOP::EncapStrToIor(const CORBA::Char *str,
     l <<= buf;
     if (l > buf.unRead())
       throw CORBA::MARSHAL(0,CORBA::COMPLETED_NO);
-    type_id = new CORBA::Char[l];
-    if (!type_id)
-      throw CORBA::NO_MEMORY(0,CORBA::COMPLETED_NO);
-    buf.get_char_array(type_id,l);
+
+    switch (l) {
+
+    case 0:
+#ifdef NO_SLOPPY_NIL_REFERENCE
+      throw CORBA::MARSHAL(0,CORBA::COMPLETED_NO);
+#else
+      // According to the CORBA specification 2.0 section 10.6.2:
+      //   Null object references are indicated by an empty set of
+      //   profiles, and by a NULL type ID (a string which contain
+      //   only *** a single terminating character ***).
+      //
+      // Therefore the idlen should be 1.
+      // Visibroker for C++ (Orbeline) 2.0 Release 1.51 gets it wrong
+      // and sends out a 0 len string.
+      // We quietly accept it here. Turn this off by defining
+      //   NO_SLOPPY_NIL_REFERENCE
+      type_id = new CORBA::Char[1];
+      type_id[0] = (CORBA::Char)'\0';
+#endif	
+      break;
+
+    case 1:
+      type_id = new CORBA::Char[1];
+      buf.get_char_array(type_id,1);
+      if (type_id[0] != (CORBA::Char)'\0')
+	throw CORBA::MARSHAL(0,CORBA::COMPLETED_NO);
+      break;
+
+    default:
+      type_id = new CORBA::Char[l];
+      if (!type_id)
+	throw CORBA::NO_MEMORY(0,CORBA::COMPLETED_NO);
+      buf.get_char_array(type_id,l);
+    }
 
     profiles = new IOP::TaggedProfileList;
     if (!profiles)
