2e4d7af15281e1470631ad75231fb8c19e310184
sll <sll>
1997-12-09 20:18:57 +0000

    Removed reference to IIOP::EncapStreamToProfile and IIOP::profileToEncapStream. Replaced macro __NT__ with __WIN32__

--- src/appl/utils/convertior/convertior.cc
+++ src/appl/utils/convertior/convertior.cc
@@ -29,6 +29,20 @@
 
 #include <omniORB2/CORBA.h>
 
+#ifndef Swap16
+#define Swap16(s) ((((s) & 0xff) << 8) | (((s) >> 8) & 0xff))
+#else
+#error "Swap16 has already been defined"
+#endif
+
+#ifndef Swap32
+#define Swap32(l) ((((l) & 0xff000000) >> 24) | \
+		   (((l) & 0x00ff0000) >> 8)  | \
+		   (((l) & 0x0000ff00) << 8)  | \
+		   (((l) & 0x000000ff) << 24))
+#else
+#error "Swap32 has already been defined"
+#endif
 
 
 static CORBA::Char* convertRef(char* old_ior, char* hostname);
@@ -40,9 +54,9 @@ static void usage(char* progname)
 
 
 
-#ifdef __NT__
+#ifdef __WIN32__
 
-// NT doesn't have an implementation of getopt() - 
+// WIN32 doesn't have an implementation of getopt() - 
 // supply a getopt() for this program:
 
 char* optarg;
@@ -104,6 +118,148 @@ getopt(int num_args, char* const* args, const char* optstring)
 #endif
 
 
+static
+void
+EncapStreamToProfile(const _CORBA_Unbounded_Sequence_Octet &s,
+			   IIOP::ProfileBody &p)
+{
+  CORBA::ULong begin = 0;
+  CORBA::ULong end = 0;
+
+  // s[0] - byteorder
+  end += 1;
+  if (s.length() <= end)
+    throw CORBA::MARSHAL(0,CORBA::COMPLETED_NO);
+
+  CORBA::Boolean byteswap = ((s[begin] == omni::myByteOrder) ? 0 : 1);
+
+  // s[1] - iiop_version.major
+  // s[2] - iiop_version.minor
+  begin = end;
+  end = begin + 2;
+  if (s.length() <= end)
+    throw CORBA::MARSHAL(0,CORBA::COMPLETED_NO);
+  p.iiop_version.major = s[begin];
+  p.iiop_version.minor = s[begin+1];
+  if (p.iiop_version.major != IIOP::current_major ||
+      p.iiop_version.minor != IIOP::current_minor)
+    throw CORBA::MARSHAL(0,CORBA::COMPLETED_NO);
+
+  // s[3] - padding
+  // s[4] - s[7] host string length
+  begin = end + 1;
+  end = begin + 4;
+  if (s.length() <= end)
+    throw CORBA::MARSHAL(0,CORBA::COMPLETED_NO);
+  {
+    CORBA::ULong len;
+    if (!byteswap) {
+      len = ((CORBA::ULong &) s[begin]);
+    }
+    else {
+      CORBA::ULong t = ((CORBA::ULong &) s[begin]);
+      len = Swap32(t);
+    }
+
+    // s[8] - s[8+len-1] host string
+    begin = end;
+    end = begin + len;
+    if (s.length() <= end)
+      throw CORBA::MARSHAL(0,CORBA::COMPLETED_NO);
+
+    // Is this string null terminated?
+    if (((char)s[end-1]) != '\0')
+      throw CORBA::MARSHAL(0,CORBA::COMPLETED_NO);
+
+    p.host = new CORBA::Char[len];
+    if (!p.host)
+      throw CORBA::NO_MEMORY(0,CORBA::COMPLETED_NO);
+    memcpy((void *)p.host,(void *)&(s[begin]),len);
+  }
+    
+  // align to CORBA::UShort
+  begin = (end + 1) & ~(1);
+  // s[begin] port number
+  end = begin + 2;
+  if (s.length() <= end)
+    throw CORBA::MARSHAL(0,CORBA::COMPLETED_NO);
+  if (!byteswap) {
+    p.port = ((CORBA::UShort &) s[begin]);
+  }
+  else {
+    CORBA::UShort t = ((CORBA::UShort &) s[begin]);
+    p.port = Swap16(t);
+  }
+
+  // align to CORBA::ULong
+  begin = (end + 3) & ~(3);
+  // s[begin]  object key length
+  end = begin + 4;
+  if (s.length() <= end)
+    throw CORBA::MARSHAL(0,CORBA::COMPLETED_NO);
+  {
+    CORBA::ULong len;
+    if (!byteswap) {
+      len = ((CORBA::ULong &) s[begin]);
+    }
+    else {
+      CORBA::ULong t = ((CORBA::ULong &) s[begin]);
+      len = Swap32(t);
+    }
+
+    begin = end;
+    end = begin + len;
+    if (s.length() < end)
+      throw CORBA::MARSHAL(0,CORBA::COMPLETED_NO);
+
+    // extract object key
+    p.object_key.length(len);
+    memcpy((void *)&p.object_key[0],(void *)&(s[begin]),len);
+  }
+  return;
+}
+
+static
+void
+profileToEncapStream(IIOP::ProfileBody &p,
+			   _CORBA_Unbounded_Sequence_Octet &s)
+{
+  CORBA::ULong hlen = strlen((const char *)p.host) + 1;
+  CORBA::ULong klen = p.object_key.length();
+  {
+    // calculate the total size of the encapsulated stream
+    CORBA::ULong total = 8 + hlen;        // first 4 bytes + aligned host
+    total = ((total + 1) & ~(1)) + 2;     // aligned port value
+    total = ((total + 3) & ~(3)) + 4 +	// aligned object key
+      klen;
+    
+    s.length(total);
+  }
+
+
+  s[0] = omni::myByteOrder;
+  s[1] = IIOP::current_major;
+  s[2] = IIOP::current_minor;
+  s[3] = 0;
+  {
+    CORBA::ULong &l = (CORBA::ULong &) s[4];
+    l = hlen;
+  }
+  memcpy((void *)&(s[8]),(void *)p.host,hlen);
+  CORBA::ULong idx = ((8 + hlen) + 1) & ~(1);
+  {
+    CORBA::UShort &l = (CORBA::UShort &) s[idx];
+    l = p.port;
+  }
+  idx = ((idx + 2) + 3) & ~(3);
+  {
+    CORBA::ULong &l = (CORBA::ULong &) s [idx];
+    l = klen;
+  }
+  idx += 4;
+  memcpy((void *)&s[idx],(void *)&p.object_key[0],klen);
+  return;
+}
 
 
 
@@ -118,7 +274,7 @@ int main(int argc, char* argv[])
 
   // Get options:
 
-#ifndef __NT__
+#ifndef __WIN32__
   extern char* optarg;
   extern int optind;
 #endif
@@ -196,11 +352,11 @@ CORBA::Char* convertRef(char* old_ior, char* hostname)
 		    IIOP::ProfileBody pBody;
 		    IOP::TaggedProfile p;
 		  
-		    IIOP::EncapStreamToProfile(
+		    EncapStreamToProfile(
 			  (*old_profiles)[count].profile_data,pBody);
 		    pBody.host = (CORBA::Char*) hostname;
 
-		    IIOP::profileToEncapStream(pBody,p.profile_data);
+		    profileToEncapStream(pBody,p.profile_data);
 		    p.tag = IOP::TAG_INTERNET_IOP;
 
 		    (*new_profiles)[count] = p;
