6555fee50d48d81f66d3bf1db19fafafcbfe14bc
sll <sll>
1997-12-09 18:06:37 +0000

    Added support for system exception handlers.

--- src/lib/omniORB2/orbcore/exception.cc
+++ src/lib/omniORB2/orbcore/exception.cc
@@ -29,6 +29,9 @@
  
 /*
   $Log$
+  Revision 1.4  1997/12/09 18:06:37  sll
+  Added support for system exception handlers.
+
   Revision 1.3  1997/08/21 22:03:56  sll
   Added system exception TRANSACTION_REQUIRED, TRANSACTION_ROLLEDBACK,
   INVALID_TRANSACTION, WRONG_TRANSACTION.
@@ -40,6 +43,276 @@
   */
 
 #include <omniORB2/CORBA.h>
+#include <excepthandler.h>
+
+
+omniORB::transientExceptionHandler_t
+omni_globalTransientExcHandler = omni_defaultTransientExcHandler;
+
+omniORB::commFailureExceptionHandler_t
+omni_globalCommFailureExcHandler = omni_defaultCommFailureExcHandler;
+
+omniORB::systemExceptionHandler_t
+omni_globalSystemExcHandler = omni_defaultSystemExcHandler;
+
+void* 
+omni_globalTransientExcHandlerCookie = 0;
+
+void*
+omni_globalCommFailureExcHandlerCookie =0;
+
+void*
+omni_globalSystemExcHandlerCookie = 0;
+
+CORBA::ULong
+omniORB::defaultTransientRetryDelayMaximum = 30;
+
+CORBA::ULong
+omniORB::defaultTransientRetryDelayIncrement = 1;
+
+omniExHandlers**
+omniExHandlers::Table = 0;
+
+omni_mutex
+omniExHandlers::TableLock;
+
+
+CORBA::Boolean
+omni_defaultTransientExcHandler(void*,
+				CORBA::ULong n_retries,
+				const CORBA::TRANSIENT& ex)
+{
+  if (omniORB::traceLevel > 10) {
+    cerr << "omniORB::defaultTransientExceptionHandler: retry "
+	 << n_retries << "th times." << endl;
+  }
+
+  unsigned long secs = n_retries*omniORB::defaultTransientRetryDelayIncrement;
+  if (secs > omniORB::defaultTransientRetryDelayMaximum) {
+    secs = omniORB::defaultTransientRetryDelayMaximum;
+  }
+  if (secs)
+    omni_thread::sleep(secs,0);
+  return 1;
+}
+
+
+void
+omniORB::installTransientExceptionHandler(void* cookie,
+			       omniORB::transientExceptionHandler_t fn)
+{
+  omni_globalTransientExcHandler = fn;
+  omni_globalTransientExcHandlerCookie = cookie;
+}
+
+void
+omniORB::installTransientExceptionHandler(CORBA::Object_ptr obj,
+				   void* cookie,
+				   omniORB::transientExceptionHandler_t fn)
+{
+  if (CORBA::is_nil(obj)) 
+    return;
+  obj->PR_getobj()->_transientExceptionHandler((void*)fn,cookie);
+}
+
+
+CORBA::Boolean
+omni_defaultCommFailureExcHandler(void*,
+				   CORBA::ULong,
+				   const CORBA::COMM_FAILURE&)
+{
+  return 0;
+}
+
+
+void
+omniORB::installCommFailureExceptionHandler(void* cookie,
+				  omniORB::commFailureExceptionHandler_t fn)
+{
+  omni_globalCommFailureExcHandler = fn;
+  omni_globalCommFailureExcHandlerCookie = cookie;
+}
+
+void
+omniORB::installCommFailureExceptionHandler(CORBA::Object_ptr obj,
+				   void* cookie,
+				   omniORB::commFailureExceptionHandler_t fn)
+{
+  if (CORBA::is_nil(obj)) 
+    return;
+  obj->PR_getobj()->_commFailureExceptionHandler((void*)fn,cookie);
+}
+
+CORBA::Boolean
+omni_defaultSystemExcHandler(void*,
+			     CORBA::ULong,
+			     const CORBA::SystemException&)
+{	
+  return 0;
+}
+
+
+void
+omniORB::installSystemExceptionHandler(void* cookie,
+				       omniORB::systemExceptionHandler_t fn)
+{
+ omni_globalSystemExcHandler = fn;
+ omni_globalSystemExcHandlerCookie = cookie;
+}
+
+void
+omniORB::installSystemExceptionHandler(CORBA::Object_ptr obj,
+				       void* cookie,
+				       omniORB::systemExceptionHandler_t fn)
+{
+  if (CORBA::is_nil(obj)) 
+    return;
+  obj->PR_getobj()->_systemExceptionHandler((void*)fn,cookie);
+}
+
+
+CORBA::Boolean
+_omni_callTransientExceptionHandler(omniObject* obj,
+				    CORBA::ULong nretries,
+				    const CORBA::TRANSIENT& ex)
+{
+  void* cookie = 0;
+  void* handler = obj->_transientExceptionHandler(cookie);
+  if (handler) {
+    return (*(omniORB::transientExceptionHandler_t)handler)(cookie,
+							    nretries,
+							    ex);
+  }
+  else {
+    return (*omni_globalTransientExcHandler)(cookie,
+					     nretries,
+					     ex);
+  }
+}
+
+CORBA::Boolean
+_omni_callCommFailureExceptionHandler(omniObject* obj,
+				      CORBA::ULong nretries,
+				      const CORBA::COMM_FAILURE& ex)
+{
+  void* cookie = 0;
+  void* handler = obj->_commFailureExceptionHandler(cookie);
+  if (handler) {
+    return (*(omniORB::commFailureExceptionHandler_t)handler)(cookie,
+							      nretries,
+							      ex);
+  }
+  else {
+    return (*omni_globalCommFailureExcHandler)(cookie,
+					       nretries,
+					       ex);
+  }
+}
+
+CORBA::Boolean
+_omni_callSystemExceptionHandler(omniObject* obj,
+				 CORBA::ULong nretries,
+				 const CORBA::SystemException& ex)
+{
+  void* cookie = 0;
+  void* handler = obj->_systemExceptionHandler(cookie);
+  if (handler) {
+    return (*(omniORB::systemExceptionHandler_t)handler)(cookie,
+							 nretries,
+							 ex);
+  }
+  else {
+    return (*omni_globalSystemExcHandler)(cookie,
+					  nretries,
+					  ex);
+  }
+}
+
+#ifndef EXHANDLER_HASH  
+#define EXHANDLER_HASH(p) ((omni::ptr_arith_t)p % omniORB::hash_table_size)
+#else
+#error "EXHANDLER_HASH has already been defined"
+#endif
+
+omniExHandlers::omniExHandlers() : 
+  transient_hdr(0), transient_cookie(0),
+  commfail_hdr(0), commfail_cookie(0), 
+  sysexcpt_hdr(0), sysexcpt_cookie(0),
+  objptr(0), next(0)
+{
+}
+
+
+omniExHandlers_iterator::omniExHandlers_iterator()
+{
+  omniExHandlers::TableLock.lock();
+}
+
+omniExHandlers_iterator::~omniExHandlers_iterator()
+{
+  omniExHandlers::TableLock.unlock();
+}
+
+omniExHandlers*
+omniExHandlers_iterator::find_or_create(omniObject* p)
+{
+  if (omniExHandlers::Table == 0) {
+    omniExHandlers::Table = new omniExHandlers* [omniORB::hash_table_size];
+    unsigned int i;
+    for (i=0; i<omniORB::hash_table_size; i++)
+      omniExHandlers::Table[i] = 0;
+  }
+  omniExHandlers* exlist = omniExHandlers::Table[EXHANDLER_HASH(p)];
+  while (exlist) {
+    if (exlist->objptr == p)
+      break;
+    exlist = exlist->next;
+  }
+  if (exlist) {
+    return exlist;
+  }
+  else {
+    omniExHandlers* np = new omniExHandlers;
+    np->objptr = p;
+    omniExHandlers** exlistp = &(omniExHandlers::Table[EXHANDLER_HASH(p)]);
+    np->next = *exlistp;
+    *exlistp = np;
+    return np;
+  }
+}
+
+omniExHandlers*
+omniExHandlers_iterator::find(omniObject* p)
+{
+  if (omniExHandlers::Table == 0) return 0;
+  omniExHandlers* exlist = omniExHandlers::Table[EXHANDLER_HASH(p)];
+  while (exlist) {
+    if (exlist->objptr == p)
+      break;
+    exlist = exlist->next;
+  }
+  return exlist;
+}
+
+void
+omniExHandlers_iterator::remove(omniObject* p)
+{
+  if (omniExHandlers::Table == 0) return;
+  omniExHandlers** exlistp = &(omniExHandlers::Table[EXHANDLER_HASH(p)]);
+  while (*exlistp) {
+    if ((*exlistp)->objptr == p) {
+      omniExHandlers* exp = *exlistp;
+      *exlistp = exp->next;
+      delete exp;
+      break;
+    }
+    else {
+      exlistp = &((*exlistp)->next);
+    }
+  }
+}
+
+#undef EXHANDLER_HASH
 
 
 CORBA::Boolean 
