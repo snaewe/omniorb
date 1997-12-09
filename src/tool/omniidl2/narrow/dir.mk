e58a4e9f21a07565c2852a3b4b53d6650d1117cc
sll <sll>
1997-12-09 20:02:58 +0000

    Removed WIN32 specific compiler flag.

--- src/tool/omniidl2/narrow/dir.mk
+++ src/tool/omniidl2/narrow/dir.mk
@@ -7,14 +7,6 @@ OBJS   = \
 
 DIR_CPPFLAGS = -I../include $(patsubst %,-I%/../include,$(VPATH))
 
-ifdef Win32Platform
-# Note the use of -W0 in CXXOPTIONS - this is used to stop pragma warning 
-# messages - should replace with a warning(disable,4068) pragma in the source
-# code
-DIR_CPPFLAGS += -D "NDEBUG" -D "_WINDOWS" -D "_X86_" -D "NTArchitecture" 
-CXXOPTIONS = -ML -W0 -GX -O2 
-endif
-
 lib = $(patsubst %,$(LibPattern),narrow)
 
 all:: $(lib)
