fbff4dc5f0e54be405fa0e16748e32793d82ada5
ewc <ewc>
1998-02-10 16:07:44 +0000

    Initial revision

---
+++ src/examples/anyExample/anyExample_impl.cc
@@ -0,0 +1,192 @@
+// anyExample_impl.cc - This is the source code of the example used in 
+//                      Chapter 9 "Type Any and TypeCode" of the omniORB2 
+//                      users guide.
+//
+//               This is the object implementation.
+//
+// Usage: anyExample_impl
+//
+//        On startup, the object reference is registered with the 
+//        COS naming service. The client uses the naming service to
+//        locate this object.
+//
+//        The name which the object is bound to is as follows:
+//              root  [context]
+//               |
+//              text  [context] kind [my_context]
+//               |
+//              anyExample  [object]  kind [Object]
+//
+
+#include <iostream.h>
+
+#include "anyExample.hh"
+
+
+static CORBA::Boolean bindObjectToName(CORBA::ORB_ptr,CORBA::Object_ptr);
+
+
+class anyExample_i : public virtual _sk_anyExample {
+public:
+  anyExample_i() { }
+  virtual ~anyExample_i() { }
+  virtual CORBA::Any* testOp(const CORBA::Any& a);
+};
+
+
+CORBA::Any* 
+anyExample_i::testOp(const CORBA::Any& a) {
+
+  cout << "Any received, containing: " << endl;
+  
+#ifndef NO_FLOAT
+  CORBA::Double d;
+#endif
+
+  CORBA::Long l;
+  char* str;
+
+  testStruct* tp;
+
+
+  if (a >>= l) {
+    cout << "Long: " << l << endl;
+  }
+#ifndef NO_FLOAT
+  else if (a >>= d) {
+    cout << "Double: " << d << endl;
+  }
+#endif
+  else if (a >>= str) {
+    cout << "String: " << str << endl;
+  }
+  else if (a >>= tp) {
+    cout << "testStruct: l: " << tp->l << endl;
+    cout << "            s: " << tp->s << endl;
+  }
+  else {
+    cout << "Unknown value." << endl;
+  }
+
+  CORBA::Any* ap = new CORBA::Any;
+  
+  *ap <<= (CORBA::ULong) 314;
+
+  cout << "Returning Any containing: ULong: 314\n" << endl;
+  return ap;
+}
+
+
+int
+main(int argc, char **argv)
+{
+  CORBA::ORB_ptr orb = CORBA::ORB_init(argc,argv,"omniORB2");
+  CORBA::BOA_ptr boa = orb->BOA_init(argc,argv,"omniORB2_BOA");
+
+  anyExample_i *myobj = new anyExample_i();
+  myobj->_obj_is_ready(boa);
+
+  {
+    anyExample_var myobjRef = myobj->_this();
+    if (!bindObjectToName(orb,myobjRef)) {
+      return 1;
+    }
+  }
+
+  boa->impl_is_ready();
+  // Tell the BOA we are ready. The BOA's default behaviour is to block
+  // on this call indefinitely.
+
+  return 0;
+}
+
+
+static
+CORBA::Boolean
+bindObjectToName(CORBA::ORB_ptr orb,CORBA::Object_ptr obj)
+{
+  CosNaming::NamingContext_var rootContext;
+  
+  try {
+    // Obtain a reference to the root context of the Name service:
+    CORBA::Object_var initServ;
+    initServ = orb->resolve_initial_references("NameService");
+
+    // Narrow the object returned by resolve_initial_references()
+    // to a CosNaming::NamingContext object:
+    rootContext = CosNaming::NamingContext::_narrow(initServ);
+    if (CORBA::is_nil(rootContext)) 
+      {
+        cerr << "Failed to narrow naming context." << endl;
+        return 0;
+      }
+  }
+  catch(CORBA::ORB::InvalidName& ex) {
+    cerr << "Service required is invalid [does not exist]." << endl;
+    return 0;
+  }
+
+
+  try {
+    // Bind a context called "test" to the root context:
+
+    CosNaming::Name contextName;
+    contextName.length(1);
+    contextName[0].id   = (const char*) "test";    // string copied
+    contextName[0].kind = (const char*) "my_context"; // string copied    
+    // Note on kind: The kind field is used to indicate the type
+    // of the object. This is to avoid conventions such as that used
+    // by files (name.type -- e.g. test.ps = postscript etc.)
+
+    CosNaming::NamingContext_var testContext;
+    try {
+      // Bind the context to root, and assign testContext to it:
+      testContext = rootContext->bind_new_context(contextName);
+    }
+    catch(CosNaming::NamingContext::AlreadyBound& ex) {
+      // If the context already exists, this exception will be raised.
+      // In this case, just resolve the name and assign testContext
+      // to the object returned:
+      CORBA::Object_var tmpobj;
+      tmpobj = rootContext->resolve(contextName);
+      testContext = CosNaming::NamingContext::_narrow(tmpobj);
+      if (CORBA::is_nil(testContext)) {
+        cerr << "Failed to narrow naming context." << endl;
+        return 0;
+      }
+    } 
+
+    // Bind the object (obj) to testContext, naming it anyExample:
+    CosNaming::Name objectName;
+    objectName.length(1);
+    objectName[0].id   = (const char*) "anyExample";   // string copied
+    objectName[0].kind = (const char*) "Object"; // string copied
+
+
+    // Bind obj with name anyExample to the testContext:
+    try {
+      testContext->bind(objectName,obj);
+    }
+    catch(CosNaming::NamingContext::AlreadyBound& ex) {
+      testContext->rebind(objectName,obj);
+    }
+    // Note: Using rebind() will overwrite any Object previously bound 
+    //       to /test/anyExample with obj.
+    //       Alternatively, bind() can be used, which will raise a
+    //       CosNaming::NamingContext::AlreadyBound exception if the name
+    //       supplied is already bound to an object.
+  }
+  catch (CORBA::COMM_FAILURE& ex) {
+    cerr << "Caught system exception COMM_FAILURE, unable to contact the "
+         << "naming service." << endl;
+    return 0;
+  }
+  catch (omniORB::fatalException& ex) {
+    throw;
+  }
+  catch (...) {
+    cerr << "Caught a system exception while using the naming service."<< endl;
+    return 0;
+  }
+  return 1;
+}
---
+++ src/examples/anyExample/dir.mk
@@ -0,0 +1,27 @@
+CXXSRCS = anyExample_impl.cc anyExample_clt.cc
+
+DIR_CPPFLAGS = $(CORBA_CPPFLAGS)
+
+CORBA_INTERFACES = anyExample
+
+anyExample_impl   = $(patsubst %,$(BinPattern),anyExample_impl)
+anyExample_clt    = $(patsubst %,$(BinPattern),anyExample_clt)
+
+all:: $(anyExample_impl) $(anyExample_clt)
+
+clean::
+	$(RM) $(anyExample_impl) $(anyExample_clt)
+
+export::$(anyExample_impl) 
+	@$(ExportExecutable)
+	@(packages="anyExample"; $(ExportATMosPackages))
+
+export:: $(anyExample_clt)
+	@$(ExportExecutable)
+
+$(anyExample_impl): anyExample_impl.o $(CORBA_STUB_OBJS) $(CORBA_LIB_DEPEND)
+	@(libs="$(CORBA_LIB)"; $(CXXExecutable))
+
+$(anyExample_clt): anyExample_clt.o $(CORBA_STUB_OBJS) $(CORBA_LIB_DEPEND)
+	@(libs="$(CORBA_LIB)"; $(CXXExecutable))
+
