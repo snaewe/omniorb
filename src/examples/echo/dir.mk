d0452b98b555c817ce6115eaa5b3e4b4073e2432
sll <sll>
1998-05-20 18:25:49 +0000

    Added new example eg3_tieimpl.cc. Added -t to invoke omniidl2.

--- src/examples/echo/dir.mk
+++ src/examples/echo/dir.mk
@@ -6,18 +6,22 @@ DIR_CPPFLAGS = $(CORBA_CPPFLAGS)
 
 CORBA_INTERFACES = echo
 
+# -t tells the compiler to generate tie implementation template
+OMNIORB2_IDL += -t
+
 eg1        = $(patsubst %,$(BinPattern),eg1)
 eg2_impl   = $(patsubst %,$(BinPattern),eg2_impl)
 eg2_clt    = $(patsubst %,$(BinPattern),eg2_clt)
 eg3_impl   = $(patsubst %,$(BinPattern),eg3_impl)
 eg3_clt    = $(patsubst %,$(BinPattern),eg3_clt)
+eg3_tieimpl = $(patsubst %,$(BinPattern),eg3_tieimpl)
 
-all:: $(eg1) $(eg2_impl) $(eg2_clt)  $(eg3_impl) $(eg3_clt)
+all:: $(eg1) $(eg2_impl) $(eg2_clt)  $(eg3_impl) $(eg3_clt) $(eg3_tieimpl)
 
 clean::
-	$(RM) $(eg1) $(eg2_impl) $(eg2_clt) $(eg3_impl) $(eg3_clt)
+	$(RM) $(eg1) $(eg2_impl) $(eg2_clt) $(eg3_impl) $(eg3_clt) $(eg3_tieimpl)
 
-export:: $(eg1) $(eg2_impl) $(eg2_clt) $(eg3_impl) $(eg3_clt)
+export:: $(eg1) $(eg2_impl) $(eg2_clt) $(eg3_impl) $(eg3_clt) $(eg3_tieimpl)
 	@(module="echoexamples"; $(ExportExecutable))
 
 $(eg1): eg1.o $(CORBA_STUB_OBJS) $(CORBA_LIB_DEPEND)
@@ -35,3 +39,5 @@ $(eg3_impl): eg3_impl.o $(CORBA_STUB_OBJS) $(CORBA_LIB_DEPEND)
 $(eg3_clt): eg3_clt.o $(CORBA_STUB_OBJS) $(CORBA_LIB_DEPEND)
 	@(libs="$(CORBA_LIB)"; $(CXXExecutable))
 
+$(eg3_tieimpl): eg3_tieimpl.o $(CORBA_STUB_OBJS) $(CORBA_LIB_DEPEND)
+	@(libs="$(CORBA_LIB)"; $(CXXExecutable))
