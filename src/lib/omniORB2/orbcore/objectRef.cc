c83ccbd58632a03df7c31ccc7439568ed26c81e7
djr <djr>
1999-01-11 09:55:53 +0000

    *** empty log message ***

--- src/lib/omniORB2/orbcore/objectRef.cc
+++ src/lib/omniORB2/orbcore/objectRef.cc
@@ -29,6 +29,9 @@
  
 /*
   $Log$
+  Revision 1.21  1999/01/11 09:55:53  djr
+  *** empty log message ***
+
   Revision 1.20  1999/01/07 16:31:43  djr
   Fixed memory leak in CORBA::UnMarshalObjRef().
   New singleton ProxyObjectTableCleaner, which releases proxy objects
@@ -905,7 +908,7 @@ ProxyObjectTableCleaner::~ProxyObjectTableCleaner()
 	  "omniORB: WARNING - Proxy object not released.\n"
 	  "  IR ID   : " << repoId << "\n"
 	  "  RefCount: " << (*p)->getRefCount() << "\n"
-	  "  ObjRef  : " << obj_ref << "\n";
+	  "  ObjRef  : " << (char*)obj_ref << "\n";
 	omniORB::log.flush();
       }
       omniObject** next = &((*p)->pd_next);
