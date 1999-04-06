e38dd6b2e933302a70fc8020b51d6f049722b1c6
djr <djr>
1999-04-06 08:33:56 +0000

    Updated version number to 2.8.0

--- src/lib/omniORB2/lifecycle/sharedlib/dir.mk
+++ src/lib/omniORB2/lifecycle/sharedlib/dir.mk
@@ -19,7 +19,7 @@
 # corresponds to a pure bug fix release.
 #
 # 
-VERSION = 2.7.2
+VERSION = 2.8.0
 
 major_version = $(word 1,$(subst ., ,$(VERSION)))
 minor_version = $(word 2,$(subst ., ,$(VERSION)))
