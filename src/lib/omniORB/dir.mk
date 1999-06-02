43d369f8c42005edfb21249f18d0f1ba4a44537e
sll <sll>
1999-06-02 16:43:25 +0000

    Changed stub generation for ir interface.

--- src/lib/omniORB/dir.mk
+++ src/lib/omniORB/dir.mk
@@ -4,7 +4,7 @@
 SUBDIRS = orbcore dynamic lifecycle
 
 
-all:: Naming.hh bootstrap.hh ir.hh corbaidl.hh omniLifeCycle.hh
+all:: Naming.hh bootstrap.hh ir_defs.hh corbaidl_defs.hh omniLifeCycle.hh
 
 
 export:: Naming.hh
@@ -15,12 +15,20 @@ export:: bootstrap.hh
 	@(file="bootstrap.hh"; dir="$(EXPORT_TREE)/$(INCDIR)/omniORB2"; \
 		$(ExportFileToDir))
 
-export:: ir.hh
-	@(file="ir.hh"; dir="$(EXPORT_TREE)/$(INCDIR)/omniORB2"; \
+export:: ir_defs.hh
+	@(file="ir_defs.hh"; dir="$(EXPORT_TREE)/$(INCDIR)/omniORB2"; \
 		$(ExportFileToDir))
 
-export:: corbaidl.hh
-	@(file="corbaidl.hh"; dir="$(EXPORT_TREE)/$(INCDIR)/omniORB2"; \
+export:: ir_operators.hh
+	@(file="ir_operators.hh"; dir="$(EXPORT_TREE)/$(INCDIR)/omniORB2"; \
+		$(ExportFileToDir))
+
+export:: corbaidl_defs.hh
+	@(file="corbaidl_defs.hh"; dir="$(EXPORT_TREE)/$(INCDIR)/omniORB2"; \
+		$(ExportFileToDir))
+
+export:: corbaidl_operators.hh
+	@(file="corbaidl_operators.hh"; dir="$(EXPORT_TREE)/$(INCDIR)/omniORB2"; \
 		$(ExportFileToDir))
 
 export:: omniLifeCycle.hh
@@ -31,8 +39,9 @@ export:: omniLifeCycle.hh
 veryclean::
 	$(RM)	Naming.idl Naming.hh NamingSK.cc NamingDynSK.cc \
 		bootstrap.idl bootstrap.hh bootstrapSK.cc bootstrapDynSK.cc \
-		ir.idl ir.hh irSK.cc irDynSK.cc \
-		corbaidl.idl corbaidl.hh corbaidlSK.cc corbaidlDynSK.cc \
+		ir.idl ir_defs.hh ir_operators.hh irSK.cc irDynSK.cc \
+		corbaidl.idl corbaidl_defs.hh corbaidl_operators.hh \
+                corbaidlSK.cc corbaidlDynSK.cc \
 		omniLifeCycle.idl omniLifeCycle.hh omniLifeCycleSK.cc \
 			omniLifeCycleDynSK.cc
 
@@ -63,19 +72,19 @@ bootstrap.hh bootstrapSK.cc bootstrapDynSK.cc: bootstrap.idl
 	-if [ "$<" != bootstrap.idl ]; then $(CP) $< . ; fi
 	$(OMNIORB2_IDL_FPATH) bootstrap.idl
 
-ir.hh irSK.cc irDynSK.cc: ir.idl corbaidl.hh
+ir_defs.hh ir_operators.hh irSK.cc irDynSK.cc: ir.idl corbaidl_defs.hh
 	-if [ "$<" != ir.idl ]; then $(CP) $< . ; fi
-	$(OMNIORB2_IDL_FPATH) -m ir.idl
+	$(OMNIORB2_IDL_FPATH) -m -F -I. ir.idl
 
-corbaidl.hh corbaidlSK.cc corbaidlDynSK.cc: corbaidl.idl
+corbaidl_defs.hh corbaidl_operators.hh corbaidlSK.cc corbaidlDynSK.cc: corbaidl.idl
 	-if [ "$<" != corbaidl.idl ]; then $(CP) $< . ; fi
-	$(OMNIORB2_IDL_FPATH) -m corbaidl.idl
+	$(OMNIORB2_IDL_FPATH) -m -F corbaidl.idl
 
 omniLifeCycle.hh omniLifeCycleSK.cc omniLifeCycleDynSK.cc: omniLifeCycle.idl
 	-if [ "$<" != omniLifeCycle.idl ]; then $(CP) $< . ; fi
 	$(OMNIORB2_IDL_FPATH) -m omniLifeCycle.idl
 
 
-ciao:: Naming.hh bootstrap.hh ir.hh corbaidl.hh omniLifeCycle.hh
+ciao:: Naming.hh bootstrap.hh ir_defs.hh corbaidl_defs.hh omniLifeCycle.hh
 	@$(MakeSubdirs)
 
