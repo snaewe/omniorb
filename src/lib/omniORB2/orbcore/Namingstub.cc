02a63ba00ed7cc40ffea68bc83f22b5e50501718
sll <sll>
1999-06-18 21:04:38 +0000

    Initial revision

---
+++ src/lib/omniORB2/orbcore/Namingstub.cc
@@ -0,0 +1,30 @@
+// -*- Mode: C++; -*-
+//                            Package   : omniORB2
+// Namingstub.cc              Created on: 6/1999
+//                            Author    : Sai-Lai Lo (sll)
+//
+//    Copyright (C) 1999 AT&T Laboratories Cambridge
+//
+//    This file is part of the omniORB library
+//
+//    The omniORB library is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Library General Public
+//    License as published by the Free Software Foundation; either
+//    version 2 of the License, or (at your option) any later version.
+//
+//    This library is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Library General Public License for more details.
+//
+//    You should have received a copy of the GNU Library General Public
+//    License along with this library; if not, write to the Free
+//    Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  
+//    02111-1307, USA
+//
+//
+// Description:
+//
+
+#define USE_dyn_stub_in_nt_dll
+#include <../NamingSK.cc>
