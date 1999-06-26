0c30583d60c824c6d8919a4987bb3d614f404520
sll <sll>
1999-06-26 17:58:15 +0000

    Clean-up.

--- src/appl/utils/catior/dir.mk
+++ src/appl/utils/catior/dir.mk
@@ -1,7 +1,7 @@
 CXXSRCS = catior.cc
 
 CorbaImplementation = OMNIORB2
-DIR_CPPFLAGS = $(CORBA_CPPFLAGS) $(OMNIORB2_CPPFLAGS) $(OMNITHREAD_CPPFLAGS)
+DIR_CPPFLAGS = $(CORBA_CPPFLAGS)
 
 CATIOR = $(patsubst %,$(BinPattern),catior)
 
@@ -10,8 +10,8 @@ all:: $(CATIOR)
 clean::
 	$(RM) $(CATIOR)
 
-$(CATIOR): catior.o $(CORBA_LIB_DEPEND) $(OMNITHREAD_LIB_DEPEND)
-	@(libs="$(CORBA_LIB) $(OMNITHREAD_LIB)"; $(CXXExecutable))
+$(CATIOR): catior.o $(CORBA_LIB_DEPEND)
+	@(libs="$(CORBA_LIB)"; $(CXXExecutable))
 
 export:: $(CATIOR)
 	@$(ExportExecutable)
