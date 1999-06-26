daa94bec1c0d944c8880ab55b4b5a0590c2e248f
sll <sll>
1999-06-26 17:58:29 +0000

    cleanup

--- src/appl/utils/convertior/dir.mk
+++ src/appl/utils/convertior/dir.mk
@@ -1,7 +1,7 @@
 CXXSRCS = convertior.cc
 
 CorbaImplementation = OMNIORB2
-DIR_CPPFLAGS = $(CORBA_CPPFLAGS) $(OMNIORB2_CPPFLAGS) $(OMNITHREAD_CPPFLAGS)
+DIR_CPPFLAGS = $(CORBA_CPPFLAGS)
 
 CONVERTIOR = $(patsubst %,$(BinPattern),convertior)
 
@@ -10,8 +10,8 @@ all:: $(CONVERTIOR)
 clean::
 	$(RM) $(CONVERTIOR)
 
-$(CONVERTIOR): convertior.o $(CORBA_LIB_DEPEND) $(OMNITHREAD_LIB_DEPEND)
-	@(libs="$(CORBA_LIB) $(OMNITHREAD_LIB)"; $(CXXExecutable))
+$(CONVERTIOR): convertior.o $(CORBA_LIB_DEPEND)
+	@(libs="$(CORBA_LIB)"; $(CXXExecutable))
 
 export:: $(CONVERTIOR)
 	@$(ExportExecutable)
