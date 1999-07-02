f5c4d332ddaa500a0424882ab8da2913f97342d5
sll <sll>
1999-07-02 19:13:21 +0000

    typedef of a typedef of a sequence now translate to a C++ typedef. Previously, the sequence is expanded to its real type.

--- src/tool/omniidl2/omniORB2_be/o2be_sequence.cc
+++ src/tool/omniidl2/omniORB2_be/o2be_sequence.cc
@@ -27,6 +27,10 @@
 
 /*
   $Log$
+  Revision 1.26  1999/07/02 19:13:21  sll
+  typedef of a typedef of a sequence now translate to a C++ typedef.
+  Previously, the sequence is expanded to its real type.
+
   Revision 1.25  1999/06/27 16:48:38  sll
   Do not expand a sequence of sequence to a template of template of base type
   if the element sequence is a typedef. This is necessary because a sequence
@@ -761,6 +765,18 @@ o2be_sequence::produce_typecode_member(std::fstream& s)
 void
 o2be_sequence::produce_typedef_hdr(std::fstream& s, o2be_typedef* tdef)
 {
+  
+  if (tdef->base_type()->node_type() == AST_Decl::NT_typedef) {
+    o2be_typedef* decl = o2be_typedef::narrow_from_decl(tdef->base_type());
+    IND(s); s << "\ntypedef " << decl->unambiguous_name(tdef) << " "
+	      << tdef->uqname() << "\n";
+    IND(s); s << "typedef " << decl->unambiguous_name(tdef) << "_var "
+	      << tdef->uqname() << "_var;\n";
+    IND(s); s << "typedef " << decl->unambiguous_name(tdef) << "_out "
+	      << tdef->uqname() << "_out;\n\n";
+    return;
+  }
+    
   {
     // gcc requires that the marshalling operators for the element
     // be declared before the sequence template is typedef'd.
