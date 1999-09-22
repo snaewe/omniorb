ffc920ccc32bc65c9588b4d59c90a4a40a5a65d0
djr <djr>
1999-09-22 08:33:52 +0000

    Updated to use the POA.

--- src/examples/echo/eg2_impl.cc
+++ src/examples/echo/eg2_impl.cc
@@ -9,39 +9,69 @@
 //        stringified IOR. This string should be used as the argument to 
 //        eg2_clt.
 //
+
 #include <iostream.h>
-#include "omnithread.h"
-#include "echo.hh"
+#include <echo.hh>
 
-#include "echo_i.cc"
 
-int
-main(int argc, char **argv)
+class Echo_i : public virtual POA_Echo,
+	       public virtual PortableServer::RefCountServantBase
 {
-  CORBA::ORB_ptr orb = CORBA::ORB_init(argc,argv,"omniORB2");
-  CORBA::BOA_ptr boa = orb->BOA_init(argc,argv,"omniORB2_BOA");
+public:
+  inline Echo_i() {}
+  virtual ~Echo_i() {}
+  virtual char* echoString(const char* mesg);
+};
 
-  Echo_i *myobj = new Echo_i();
-  myobj->_obj_is_ready(boa);
 
-  {
-    Echo_var myobjRef = myobj->_this();
-    CORBA::String_var p = orb->object_to_string(myobjRef);
-    cerr << "'" << (char*)p << "'" << endl;
+char* Echo_i::echoString(const char* mesg)
+{
+  return CORBA::string_dup(mesg);
+}
+
+//////////////////////////////////////////////////////////////////////
+
+int main(int argc, char** argv)
+{
+  try {
+    CORBA::ORB_var orb = CORBA::ORB_init(argc, argv, "omniORB3");
+
+    CORBA::Object_var obj = orb->resolve_initial_references("RootPOA");
+    PortableServer::POA_var poa = PortableServer::POA::_narrow(obj);
+
+    Echo_i* myecho = new Echo_i();
+
+    PortableServer::ObjectId_var myechoid = poa->activate_object(myecho);
+
+    // Obtain a reference to the object, and print it out as a
+    // stringified IOR.
+    obj = myecho->_this();
+    CORBA::String_var sior(orb->object_to_string(obj));
+    cerr << "'" << (char*)sior << "'" << endl;
+
+    myecho->_remove_ref();
+
+    PortableServer::POAManager_var pman = poa->the_POAManager();
+    pman->activate();
+
+    orb->run();
+    orb->destroy();
+  }
+  catch(CORBA::SystemException&) {
+    cerr << "Caught CORBA::SystemException." << endl;
+  }
+  catch(CORBA::Exception&) {
+    cerr << "Caught CORBA::Exception." << endl;
+  }
+  catch(omniORB::fatalException& fe) {
+    cerr << "Caught omniORB::fatalException:" << endl;
+    cerr << "  file: " << fe.file() << endl;
+    cerr << "  line: " << fe.line() << endl;
+    cerr << "  mesg: " << fe.errmsg() << endl;
+  }
+  catch(...) {
+    cerr << "Caught unknown exception." << endl;
   }
 
-  boa->impl_is_ready();
-  // Tell the BOA we are ready. The BOA's default behaviour is to block
-  // on this call indefinitely.
-
-  // Call boa->impl_shutdown() from another thread would unblock the
-  // main thread from impl_is_ready().
-  //
-  // To properly shutdown the BOA and the ORB, add the following calls
-  // after impl_is_ready() returns.
-  //
-  // boa->destroy();
-  // orb->NP_destroy();
- 
   return 0;
 }
