9f26a3d83b4b6eefec4e67431eacedf8fabeb7af
sll <sll>
1999-09-23 15:17:11 +0000

    *** empty log message ***

--- src/lib/omniORB2/test.cc
+++ src/lib/omniORB2/test.cc
@@ -1,8 +1,12 @@
 // This is a test file.
 // Next modification.
+// Another modification.
 
 /*
   $Log$
+  Revision 1.1.2.3  1999/09/23 15:17:11  sll
+  *** empty log message ***
+
   Revision 1.1.2.2  1999/09/23 15:02:43  sll
   *** empty log message ***
 
