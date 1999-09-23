cb7abea34eb9f7bf241f6a2c358515aa22da68ef
sll <sll>
1999-09-23 19:06:20 +0000

    Added static functions stacksize() in the omnithread library to control the stack size when spawning a new thread.

--- include/omnithread.h
+++ include/omnithread.h
@@ -445,6 +445,13 @@ public:
 	// use in timed_waits on condition variables, which is the current time
 	// plus the given relative offset.
 
+
+    static void stacksize(unsigned long sz=0);
+    static unsigned long stacksize();
+        // Use this value as the stack size when spawning a new thread.
+        // The default value (0) means that the thread library default is
+        // to be used.
+
 private:
 
     virtual void run(void* arg) {}
