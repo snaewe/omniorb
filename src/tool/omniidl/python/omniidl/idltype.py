a020b0c232626f2f626b4ac62d092ca8e1c11e50
dpg1 <dpg1>
1999-11-15 14:57:34 +0000

    Added copyright notice.

--- src/tool/omniidl/python/omniidl/idltype.py
+++ src/tool/omniidl/python/omniidl/idltype.py
@@ -1,6 +1,30 @@
+# -*- python -*-
+#                           Package   : omniidl
+# idltype.py                Created on: 1999/10/27
+#			    Author    : Duncan Grisby (dpg1)
 #
-# IDL type representation
+#    Copyright (C) 1999 AT&T Laboratories Cambridge
 #
+#  This file is part of omniidl.
+#
+#  omniidl is free software; you can redistribute it and/or modify it
+#  under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+#  02111-1307, USA.
+#
+# Description:
+#   
+#   IDL type representation
 
 import idlutil
 
