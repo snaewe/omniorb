b7711607b8a014bc713d901ead910e45cb284d9e
djs <djs>
1999-11-24 18:01:02 +0000

    New module to handle operation dispatching

---
+++ src/lib/omniORB/omniidl_be/cxx/skel/dispatch.py
@@ -0,0 +1,289 @@
+# -*- python -*-
+#                           Package   : omniidl
+# dispatch.py               Created on: 1999/11/24
+#			    Author    : David Scott (djs)
+#
+#    Copyright (C) 1999 AT&T Laboratories Cambridge
+#
+#  This file is part of omniidl.
+#
+#  omniidl is free software; you can redistribute it and/or modify it
+#  under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+#  02111-1307, USA.
+#
+# Description:
+#
+#   Produce operation and attribute call dispatching code
+
+# $Id$
+# $Log$
+# Revision 1.1  1999/11/24 18:01:02  djs
+# New module to handle operation dispatching
+#
+
+"""Produce operation and attribute call dispatching code"""
+
+import string
+
+from omniidl import idlutil, idltype, idlast
+
+from omniidl.be.cxx import util, tyutil, skutil
+
+
+
+import dispatch
+self = dispatch
+
+def __init__(environment, stream):
+    self.__environment = environment
+    self.__stream = stream
+    return self
+
+
+# Create appropriate argument type instances
+#  -out- (includes return) types have their storage
+#        created here (eg by _var mapping)
+#  -in-, -inout- types have had their storage
+#        allocated by the caller
+def argument_instance(type):
+    assert isinstance(type, idltype.Type)
+    environment = self.__environment
+    deref_type = tyutil.deref(type)
+    dims = tyutil.typeDims(deref_type)
+    
+    is_variable = tyutil.isVariableType(deref_type)
+    is_array = dims != []
+
+    # in, out, inout (corresponds to idltype.Parameter.direction()
+    mapping = ["", "", ""]
+
+    # typedefs aren't dereferenced
+    if tyutil.isTypedef(type):
+        name = environment.principalID(type)
+        mapping[0] = name
+        # out types have storage allocated here
+        if is_variable:
+            mapping[1] = name + "_var"
+        else:
+            mapping[1] = name
+        mapping[2] = name
+        
+        return mapping
+
+    # all object references are _var types
+    if tyutil.isObjRef(deref_type):
+        name = environment.principalID(type)
+        mapping = [name + "_var", name + "_var", name + "_var"]
+
+        return mapping
+        
+    # all strings are CORBA::String_var
+    if tyutil.isString(deref_type):
+        mapping = ["CORBA::String_var", "CORBA::String_var",
+                   "CORBA::String_var"]
+        return mapping
+
+    name = environment.principalID(type)
+    mapping = [name, name, name]
+    if is_variable:
+        mapping[1] = name + "_var"
+
+    return mapping
+
+
+# Given an argument type and a name, returns the thing actually
+# passed to the method call.
+def method_argument(type, name):
+    assert isinstance(type, idltype.Type)
+    environment = self.__environment
+    deref_type = tyutil.deref(type)
+    type_dims = tyutil.typeDims(type)
+    is_array = type_dims != []
+    is_variable = tyutil.isVariableType(deref_type)
+    
+    # in, out, inout (corresponds to idltype.Parameter.direction()
+    mapping = ["", "", ""]
+
+    # arrays
+    if is_array and is_variable:
+        mapping = [name, name + ".out()", name]
+        return mapping
+
+    # strings and object references
+    if tyutil.isString(deref_type) or tyutil.isObjRef(deref_type):
+        mapping = [name + ".in()", name + ".out()", name + ".inout()"]
+        return mapping
+
+    is_variable = tyutil.isVariableType(deref_type)
+    mapping = [name, name, name]
+    if is_variable:
+        mapping[1] = name + ".out()"
+
+    return mapping
+        
+
+# Given an argument type and a name, indicates whether the thing
+# is passed by a pointer (involves .operator->() etc)
+def is_pointer(type):
+    assert isinstance(type, idltype.Type)
+    deref_type = tyutil.deref(type)
+
+    # strings are a special case
+    if tyutil.isString(deref_type):
+        return 0
+
+    is_variable = tyutil.isVariableType(deref_type)
+    if is_variable:
+        return 1
+
+    return 0
+
+def operation(operation):
+    environment = self.__environment
+    stream = self.__stream
+    operation_name = tyutil.mapID(operation.identifier())
+
+    return_type = operation.returnType()
+    deref_return_type = tyutil.deref(return_type)
+    return_type_dims = tyutil.typeDims(return_type)
+    has_return_value = not(tyutil.isVoid(return_type))
+
+    # all arguments are prefixed, return value is called "return"
+    prefix = "arg_"
+    
+    # create the operation arguments and demarshal them
+    # if values are supplied by the caller (ie in or inout)
+    get_arguments = util.StringStream()
+
+    # names of arguments to pass to the method
+    argument_list = []
+
+    # declare the result type if necessary and call the method
+    decl_result = util.StringStream()
+
+    # compute the message size for alignment handling
+    size_calc_arguments = util.StringStream()
+    size_calc_results = util.StringStream()
+
+    # marshal the result (if any)
+    put_results = util.StringStream()
+
+    # marshal the out arguments (if any)
+    put_arguments = util.StringStream()
+
+    for argument in operation.parameters():
+        argument_name = tyutil.mapID(argument.identifier())
+        argument_prefixed_name = prefix + argument_name
+        direction = argument.direction()
+        argument_type = argument.paramType()
+        deref_argument_type = tyutil.deref(argument_type)
+
+        argument_type_names = argument_instance(argument_type)
+        # declare the argument
+        get_arguments.out("""\
+@argument_type_name@ @argument_name@;""",
+                          argument_type_name = argument_type_names[direction],
+                          argument_name = argument_prefixed_name)
+        # consider the need to demarshal it
+        if argument.is_in():
+            skutil.unmarshall(get_arguments, environment, argument_type,
+                              None, prefix + argument_name, 1, "giop_s")
+
+        is_pntr = is_pointer(argument_type)
+        if is_pntr:
+            argument_operator_name = "(" + argument_prefixed_name +\
+                                     ".operator->())"
+
+        # and consider the need to marshal it again
+        if argument.is_out():
+            name = argument_prefixed_name
+            if is_pntr and direction == 1:
+                name = argument_operator_name
+                
+            skutil.marshall(put_arguments, environment, argument_type,
+                            None, name, "giop_s")
+
+            size_calc_arguments.out(
+                skutil.sizeCalculation(environment, argument_type,
+                                       None, "msgsize",
+                                       name,
+                                       is_pointer = is_pntr,
+                                       fixme = 1))
+        # build the argument list
+        argument_list.append(method_argument(argument_type,
+                                             argument_prefixed_name)[direction])
+        
+    # by default there is no result to assign method return value to    
+    result_assignment = ""
+    
+    if has_return_value:
+        return_is_array = return_type_dims != []
+        return_is_variable = tyutil.isVariableType(return_type)
+        result_mapping = argument_instance(return_type)[1]
+
+        # exception- arrays of fixed types use the _var mapping
+        if not(return_is_variable) and return_is_array:
+            result_mapping = result_mapping + "_var"
+            
+        decl_result.out("""\
+@result_type@ result;""", result_type = result_mapping)
+
+        name = "result"
+        if is_pointer(return_type):
+            name = "(result.operator->())"
+        # needs to be counted in the message size calculation
+        size_calc_results.out(
+            skutil.sizeCalculation(environment, return_type,
+                                   None, "msgsize", name,
+                                   fixme = 1))
+        skutil.marshall(put_results, environment, return_type, None,
+                        name, "giop_s")
+        result_assignment = "result = "
+
+
+    # main block of code goes here
+    stream.out("""\
+  if( !strcmp(giop_s.operation(), \"@operation_name@\") ) {
+    @get_arguments@
+    giop_s.RequestReceived();
+    @decl_result@
+    @result_assignment@this->@operation_name@(@argument_list@);
+    if( giop_s.response_expected() ) {
+      size_t msgsize = (size_t) GIOP_S::ReplyHeaderSize();
+      @size_calculation_results@
+      @size_calculation_arguments@
+      giop_s.InitialiseReply(GIOP::NO_EXCEPTION, (CORBA::ULong) msgsize);
+      @put_results@
+      @put_arguments@
+    }
+    giop_s.ReplyCompleted();
+    return 1;
+  }""",
+               operation_name = operation_name,
+               get_arguments = str(get_arguments),
+               decl_result = str(decl_result),
+               argument_list = string.join(argument_list, ", "),
+               result_assignment = result_assignment,
+               size_calculation_results = str(size_calc_results),
+               size_calculation_arguments = str(size_calc_arguments),
+               put_results = str(put_results),
+               put_arguments = str(put_arguments))
+        
+        
+
+        
+
+
+def attribute(operation):
+    pass
