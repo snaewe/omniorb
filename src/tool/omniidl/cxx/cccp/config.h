3c700259be833fbb0d99c579646c0106888835b9
dpg1 <dpg1>
2000-02-21 14:08:38 +0000

    Added guessed make rules for HPUX, IRIX, OSF1 and Sinix.

---
+++ src/tool/omniidl/cxx/cccp/config.h
@@ -0,0 +1,49 @@
+#if defined(__linux__)
+#include "config-linux.h"
+
+#elif defined(__sunos__)
+#include "config-solaris.h"
+
+#elif defined(__aix__)
+#include "config-aix.h"
+
+#elif defined(__WIN32__)
+#include "config-windows.h"
+
+#elif defined(__freebsd__)
+#include "config-freebsd.h"
+
+#elif defined(__hpux__)
+#include "config-hpux.h"
+
+#elif defined(__VMS)
+#include "config-vms.h"
+
+#elif defined(__osf1__)
+#include "config-osf1.h"
+
+#elif defined(__SINIX__)
+#include "config-sinix.h"
+
+#elif defined(__irix__)
+#include "config-irix.h"
+
+#endif
+
+#define BITS_PER_UNIT SIZEOF_UNSIGNED_CHAR
+#define BITS_PER_WORD SIZEOF_INT
+#define HOST_BITS_PER_INT SIZEOF_INT
+
+#define TARGET_BELL '\a'
+#define TARGET_BS '\b'
+#define TARGET_FF '\f'
+#define TARGET_NEWLINE '\n'
+#define TARGET_CR '\r'
+#define TARGET_TAB '\t'
+#define TARGET_VT '\v'
+
+#define INCLUDE_DEFAULTS { { 0, 0, 0 } }
+#define GCC_INCLUDE_DIR "/usr/include"
+
+#define FATAL_EXIT_CODE 1
+#define SUCCESS_EXIT_CODE 0
--- src/tool/omniidl/cxx/dir.mk
+++ src/tool/omniidl/cxx/dir.mk
@@ -13,12 +13,13 @@ export::
 endif
 
 
-
 SUBDIRS = cccp
+
 all::
 	@$(MakeSubdirs)
 export::
 	@$(MakeSubdirs)
+
 
 OBJS = y.tab.o lex.yy.o idlerr.o idlutil.o idltype.o \
 	idlrepoId.o idlscope.o idlexpr.o idlast.o idlvalidate.o \
@@ -277,10 +278,185 @@ endif
 
 
 #############################################################################
+#   Make rules for Digital Unix                                             #
+#############################################################################
+
+# WARNING!  These make rules are untested
+
+ifdef OSF1
+ifeq ($(notdir $(CXX)),cxx)
+
+libname = _omniidlmodule.so
+soname  = $(libname).$(IDLMODULE_MAJOR)
+lib = $(soname).$(IDLMODULE_MINOR)
+
+all:: $(lib)
+
+$(lib): $(OBJS) $(PYOBJS)
+	(set -x; \
+         $(RM) $@; \
+         ld -shared -soname $(soname) -set_version $(soname) -o $@ $(IMPORT_LIBRARY_FLAGS) \
+         $(filter-out $(LibSuffixPattern),$^) -lcxxstd -lcxx -lexc -lots -lc \
+        )
+
+
+clean::
+	$(RM) $(lib)
+
+export:: $(lib)
+	@$(ExportLibrary)
+	@(set -x; \
+          cd $(EXPORT_TREE)/$(LIBDIR); \
+          $(RM) $(soname); \
+          ln -s $(lib) $(soname); \
+          $(RM) $(libname); \
+          ln -s $(soname) $(libname); \
+         )
+
+endif
+endif
+
+
+#############################################################################
+#   Make rules for HPUX                                                     #
+#############################################################################
+
+# WARNING!  These make rules are untested
+
+ifdef HPUX
+ifeq ($(notdir $(CXX)),aCC)
+
+DIR_CPPFLAGS += +Z
+
+libname = _omniidlmodule.so
+soname = $(libname).$(IDLMODULE_MAJOR)
+lib = $(soname).$(IDLMODULE_MINOR)
+
+all:: $(lib)
+
+$(lib): $(OBJS) $(PYOBJS)
+	(set -x; \
+         $(RM) $@; \
+         aCC -b -Wl,+h$(soname) -o $@  $(IMPORT_LIBRARY_FLAGS) \
+           $(patsubst %,-L %,$(IMPORT_LIBRARY_DIRS)) \
+           $(filter-out $(LibSuffixPattern),$^) ; \
+        )
+
+clean::
+	$(RM) $(lib)
+
+export:: $(lib)
+	@$(ExportLibrary)
+	@(set -x; \
+          cd $(EXPORT_TREE)/$(LIBDIR); \
+          $(RM) $(soname); \
+          ln -s $(lib) $(soname); \
+          $(RM) $(libname); \
+          ln -s $(soname) $(libname); \
+         )
+
+endif
+endif
+
+#############################################################################
+#   Make rules for to Reliant Unix                                          #
+#############################################################################
+
+# WARNING!  These make rules are untested
+
+ifdef SINIX
+ifeq ($(notdir $(CXX)),CC)
+
+DIR_CPPFLAGS += -Kpic
+
+libname = _omniidlmodule.so
+soname = $(libname).$(IDLMODULE_MAJOR)
+lib = $(soname).$(IDLMODULE_MINOR)
+
+$(lib): $(ORB_OBJS)
+	(set -x; \
+         $(RM) $@; \
+         CC -G -z text -KPIC -o $@ -h $(soname) \
+           $(IMPORT_LIBRARY_FLAGS) $($(IMPORT_LIBRARY_DIRS)) \
+           $(filter-out $(LibSuffixPattern),$^); \
+        )
+
+
+all:: $(lib)
+
+clean::
+	$(RM) $(lib)
+
+export:: $(lib)
+	@$(ExportLibrary)
+	@(set -x; \
+          cd $(EXPORT_TREE)/$(LIBDIR); \
+          $(RM) $(soname); \
+          ln -s $(lib) $(soname); \
+          $(RM) $(libname); \
+          ln -s $(soname) $(libname); \
+         )
+
+endif
+endif
+
+#############################################################################
+#   Make rules for SGI Irix 6.2                                             #
+#############################################################################
+
+# WARNING!  These make rules are untested
+
+ifdef IRIX
+ifeq ($(notdir $(CXX)),CC)
+
+DIR_CPPFLAGS += -KPIC
+
+ifdef IRIX_n32
+ADD_CPPFLAGS = -n32
+endif
+ifdef IRIX_64
+ADD_CPPFLAGS = -64
+endif
+
+libname = _omniidlmodule.so
+soname = $(libname).$(IDLMODULE_MAJOR)
+lib = $(soname).$(IDLMODULE_MINOR)
+
+all:: $(lib)
+
+$(lib): $(ORB_OBJS)
+	(set -x; \
+         $(RM) $@; \
+         $(LINK.cc) -KPIC -shared -Wl,-h,$(libname) \
+           -Wl,-set_version,$(soname) -Wl,-rpath,$(LIBDIR) \
+           -o $@ $(IMPORT_LIBRARY_FLAGS) $^ $(LDLIBS); \
+        )
+
+
+clean::
+	$(RM) $(lib)
+
+export:: $(lib)
+	@$(ExportLibrary)
+	@(set -x; \
+          cd $(EXPORT_TREE)/$(LIBDIR); \
+          $(RM) $(soname); \
+          ln -s $(lib) $(soname); \
+          $(RM) $(libname); \
+          ln -s $(soname) $(libname); \
+         )
+
+endif
+endif
+
+
+
+
+#############################################################################
 #   Test executable                                                         #
 #############################################################################
 
-all:: $(idlc)
+# all:: $(idlc)
 
-$(idlc): $(OBJS) idlc.o
-	@(libs=""; $(CXXExecutable))
+# $(idlc): $(OBJS) idlc.o
+# 	@(libs=""; $(CXXExecutable))
