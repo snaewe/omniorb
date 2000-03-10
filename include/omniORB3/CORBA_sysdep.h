64613e7fe0c92706cacc45d7091072fa5ce26d9b
dpg1 <dpg1>
2000-03-10 16:45:52 +0000

    MSVC++ can't catch exceptions by base class.

--- include/omniORB3/CORBA_sysdep.h
+++ include/omniORB3/CORBA_sysdep.h
@@ -32,6 +32,9 @@
 
 /*
  $Log$
+ Revision 1.1.2.8  2000/03/10 16:45:52  dpg1
+ MSVC++ can't catch exceptions by base class.
+
  Revision 1.1.2.7  2000/02/23 12:57:15  dpg1
  Some support for NextStep x86.
 
@@ -259,6 +262,9 @@
 #endif
 #define _HAS_NOT_GOT_strcasecmp
 #define _HAS_NOT_GOT_strncasecmp
+// No current version of MSVC++ can catch exceptions by base class
+#undef HAS_Cplusplus_catch_exception_by_base
+
 
 #elif defined(__BCPLUSPLUS__)
 #define HAS_Cplusplus_Namespace
