6ece3cbeaec46182584448913686b2df2407abd4
sll <sll>
2000-09-27 18:21:35 +0000

    Use the new GIOP engine to drive a remote call.

--- src/lib/omniORB/orbcore/giopClient.cc
+++ src/lib/omniORB/orbcore/giopClient.cc
@@ -29,6 +29,9 @@
  
 /*
   $Log$
+  Revision 1.13.2.2  2000/09/27 18:21:35  sll
+  Use the new GIOP engine to drive a remote call.
+
   Revision 1.13.2.1  2000/07/17 10:35:53  sll
   Merged from omni3_develop the diff between omni3_0_0_pre3 and omni3_0_0.
 
@@ -79,27 +82,19 @@
 //
   */
 
-#include <omniORB3/CORBA.h>
+#include <omniORB4/CORBA.h>
 
 #ifdef HAS_pch
 #pragma hdrstop
 #endif
 
-#include <scavenger.h>
 #include <exceptiondefs.h>
 
 
-GIOP_C::GIOP_C(Rope *r)
-  : NetBufferedStream(r,1,1,0)
+GIOP_C::GIOP_C(omniIOR* ior, Rope *r) : 
+  pd_state(GIOP_C::Idle), pd_ior(ior)
 {
-  pd_state = GIOP_C::Idle;
-  pd_max_message_size = NetBufferedStream::MaxMessageSize() - 
-                           sizeof(MessageHeader::HeaderType) -
-                           sizeof(CORBA::ULong);
-  if (pd_max_message_size > omniORB::MaxMessageSize()) {
-    pd_max_message_size = omniORB::MaxMessageSize();
-  }
-  return;
+  pd_cdrStream = giopStream::acquireClient(r,ior->iiop.version);
 }
 
 
@@ -113,102 +108,41 @@ GIOP_C::~GIOP_C()
     return;
   }
   if (pd_state != GIOP_C::Idle) {
-    setStrandIsDying();
+    pd_cdrStream->setTerminalError();
   }
   pd_state = GIOP_C::Zombie;
-  return;
-}
-
-size_t
-GIOP_C::RequestHeaderSize(const size_t objkeysize, const size_t opnamesize)
-{
-  // Compute the header size, this includes the GIOP Message header and
-  // the Request message header.
-
-  CORBA::ULong msgsize = sizeof(MessageHeader::Request) + sizeof(CORBA::ULong);
-
-  // RequestHeader.service_context
-  msgsize += sizeof(CORBA::ULong); // XXX 0 length service context
-
-  // RequestHeader.request_id
-  // msgsize = omni::align_to(msgsize,omni::ALIGN_4);
-  msgsize += sizeof(CORBA::ULong);
-
-  // RequestHeader.response_expected
-  // msgsize = omni::align_to(msgsize,omni::ALIGN_1);
-  msgsize += sizeof(CORBA::Boolean);
-
-  // RequestHeader.object_key
-  msgsize = omni::align_to(msgsize,omni::ALIGN_4);
-  msgsize += sizeof(CORBA::ULong) + objkeysize;
-  
-  // RequestHeader.operation
-  msgsize = omni::align_to(msgsize,omni::ALIGN_4);
-  msgsize += sizeof(CORBA::ULong) + opnamesize;
-
-  // RequestHeader.requesting_principal
-  msgsize = omni::align_to(msgsize,omni::ALIGN_4);
-  msgsize += sizeof(CORBA::ULong) + omni::myPrincipalID.length();
-
-  return msgsize;
+  pd_cdrStream->release();
 }
 
 void 
-GIOP_C::InitialiseRequest(const void          *objkey,
-			  const size_t         objkeysize,
-			  const char          *opname,
+GIOP_C::InitialiseRequest(const char*          opname,
 			  const size_t         opnamesize,
-			  const size_t         msgsize,
-			  const CORBA::Boolean oneway)
+			  const _CORBA_Boolean oneway,
+			  const _CORBA_Boolean response_expected,
+			  giopMarshaller&      marshaller)
 {
   if (pd_state != GIOP_C::Idle)
     throw omniORB::fatalException(__FILE__,__LINE__,
       "GIOP_C::InitialiseRequest() entered with the wrong state.");
  
-  clicksSet(StrandScavenger::clientCallTimeLimit());
-
-  size_t bodysize =msgsize-sizeof(MessageHeader::Request)-sizeof(CORBA::ULong);
-  if (bodysize > MaxMessageSize()) {
-    OMNIORB_THROW(MARSHAL,0,CORBA::COMPLETED_NO);
-  }
-
   pd_state = GIOP_C::RequestInProgress;
-  pd_request_id = newRequestID();
+
+  pd_response_expected = response_expected;
+
+  // Setup callback in case the giopStream have to calculate the
+  // message body size.
+  pd_cdrStream->outputMessageBodyMarshaller(marshaller);
 
 
-  // Marshall the GIOP Message Header
-  WrMessageSize(msgsize);
-  put_char_array((CORBA::Char *)MessageHeader::Request,
-		 sizeof(MessageHeader::Request),
-		 omni::ALIGN_1, 1, !oneway);
+  // Marshal request header
+  (void) pd_cdrStream->outputRequestMessageBegin(pd_ior,
+						 opname,
+						 opnamesize,
+						 oneway,
+						 response_expected);
 
-  operator>>= ((CORBA::ULong)bodysize,*this);
-
-  // Marshall the Request Header
-  // XXX should marshall service context here,
-  // XXX for the moment, just marshall a 0 length service context
-  operator>>= ((CORBA::ULong)0,*this);
-
-  operator>>= (pd_request_id,*this);
-
-  if (oneway)
-    pd_response_expected = 0;
-  else
-    pd_response_expected = 1;
-  operator>>= (pd_response_expected,*this);
-
-  operator>>= ((CORBA::ULong) objkeysize,*this);
-  put_char_array((CORBA::Char*) objkey, objkeysize);
-
-  operator>>= ((CORBA::ULong) opnamesize,*this);
-
-  put_char_array((CORBA::Char*) opname, opnamesize);
-
-  CORBA::ULong a = omni::myPrincipalID.length();
-  operator>>= (a,*this);
-
-  put_char_array((CORBA::Char*) omni::myPrincipalID.NP_data(),
-		 omni::myPrincipalID.length());
+  // Marshal request body
+  marshaller.marshal(*pd_cdrStream);
 }
 
 GIOP::ReplyStatusType 
@@ -218,79 +152,20 @@ GIOP_C::ReceiveReply()
     throw omniORB::fatalException(__FILE__,__LINE__,
       "GIOP_C::ReceiveReply() entered with the wrong state.");
 
-  if (WrMessageSpaceLeft())
-    throw omniORB::fatalException(__FILE__,__LINE__,
-       "GIOP_C::ReceiveReply() reported wrong request message size.");
-
+  pd_cdrStream->outputMessageEnd();
   pd_state = GIOP_C::WaitingForReply;
-  flush(1);
 
- if (!pd_response_expected) {
+  if (!pd_response_expected) {
     pd_state = GIOP_C::ReplyIsBeingProcessed;
     return GIOP::NO_EXCEPTION;
   }
   
-  RdMessageSize(0,omni::myByteOrder);
+  GIOP::ReplyStatusType rc;
 
-  MessageHeader::HeaderType hdr;
-  get_char_array((CORBA::Char*) hdr, sizeof(MessageHeader::HeaderType),
-		 omni::ALIGN_1, 1);
+  rc = pd_cdrStream->inputReplyMessageBegin();
 
   pd_state = GIOP_C::ReplyIsBeingProcessed;
 
-  if (hdr[0] != MessageHeader::Reply[0] ||
-      hdr[1] != MessageHeader::Reply[1] ||
-      hdr[2] != MessageHeader::Reply[2] ||
-      hdr[3] != MessageHeader::Reply[3] ||
-      hdr[4] != MessageHeader::Reply[4] ||
-      hdr[5] != MessageHeader::Reply[5] ||
-      hdr[7] != MessageHeader::Reply[7])
-    {
-      // Wrong header
-      setStrandIsDying();
-      OMNIORB_THROW_CONNECTION_BROKEN(0,CORBA::COMPLETED_MAYBE);
-    }
-
-  CORBA::ULong msgsize;
-  msgsize <<= *this;
-  if (hdr[6] != omni::myByteOrder) {
-    msgsize =  ((((msgsize) & 0xff000000) >> 24) | 
-		(((msgsize) & 0x00ff0000) >> 8)  | 
-		(((msgsize) & 0x0000ff00) << 8)  | 
-		(((msgsize) & 0x000000ff) << 24));
-  }
-
-  if (msgsize > MaxMessageSize()) {
-    // message size has exceeded the limit
-    setStrandIsDying();
-    OMNIORB_THROW_CONNECTION_BROKEN(0,CORBA::COMPLETED_MAYBE);
-  }
-
-  RdMessageSize(msgsize,hdr[6]);
-
-  // XXX Do not support any service context yet, 
-  // XXX For the moment, skips the service context
-  CORBA::ULong svcccount;
-  CORBA::ULong svcctag;
-  CORBA::ULong svcctxtsize;
-  svcccount <<= *this;
-  while (svcccount-- > 0) {
-    svcctag <<= *this;
-    svcctxtsize <<= *this;
-    skip(svcctxtsize);
-  };
-
-  CORBA::ULong req_id;
-  req_id <<= *this;
-  if (req_id != pd_request_id) {
-    // Not the expected reply, skip the entire message
-    skip(RdMessageUnRead(),1);
-    pd_state = GIOP_C::RequestInProgress;
-    return this->ReceiveReply();
-  }
-
-  CORBA::ULong rc;
-  rc <<= *this;
   switch (rc) {
   case GIOP::SYSTEM_EXCEPTION:
     {
@@ -301,14 +176,16 @@ GIOP_C::ReceiveReply()
   case GIOP::NO_EXCEPTION:
   case GIOP::USER_EXCEPTION:
   case GIOP::LOCATION_FORWARD:
+  case GIOP::LOCATION_FORWARD_PERM:
+  case GIOP::NEEDS_ADDRESSING_MODE:
     break;
   default:
     // Should never receive anything other that the above
     // Same treatment as wrong header
-    setStrandIsDying();
+    pd_cdrStream->setTerminalError();
     OMNIORB_THROW_CONNECTION_BROKEN(0,CORBA::COMPLETED_MAYBE);
   }
-  return (GIOP::ReplyStatusType)rc;
+  return rc;
 }
 
 void
@@ -317,150 +194,51 @@ GIOP_C::RequestCompleted(CORBA::Boolean skip_msg)
   if (pd_state != GIOP_C::ReplyIsBeingProcessed)
     throw omniORB::fatalException(__FILE__,__LINE__,
       "GIOP_C::RequestCompleted() entered with the wrong state.");
-  if (skip_msg)
-    {
-      skip(RdMessageUnRead(),1);
-    }
-  else
-    {
-      if (RdMessageUnRead())
-	{
-	  // The value in the message size field in the GIOP header is larger
-	  // than the actual body size, i.e there is garbage at the end.
-	  //
-	  // The default behaviour is to just silently skip the unread part.
-	  // The problem with this behaviour is that the header message size
-	  // may actually be garbage value, caused by a bug in the client
-	  // code. This thread may forever block on the strand as it tries
-	  // to read more data from it. In this case the client won't send
-	  // anymore as it thinks it has marshalled in all the data.
-	  //
-	  // It is really a sloppy IIOP implementation (through strictly
-	  // speaking not a violation of the specification) to have a
-	  // message size value in the header that doesn't match with
-	  // a body that contains only the marshalled data.
-	  //
-	  // If omniORB::strictIIOP non-zero, we expect incoming calls to
-	  // be well behaved and rejects anything that is not.
-	  if (omniORB::traceLevel >= 15) {
-	    omniORB::log << "GIOP_C::RequestCompleted: garbage left at the end of message.\n";
-	    omniORB::log.flush();
-	  }
-	  if (!omniORB::strictIIOP) {
-	    skip(RdMessageUnRead(),1);
-	  }
-	  else {
-	    setStrandIsDying();
-	    OMNIORB_THROW_CONNECTION_BROKEN(0,CORBA::COMPLETED_NO);
-	  }
-	}
-      else {
-	skip(0,1);
-      }
-    }
-  clicksSet(StrandScavenger::outIdleTimeLimit());
+
+  pd_cdrStream->inputMessageEnd(skip_msg);
   pd_state = GIOP_C::Idle;
-  return;
 }
 
 GIOP::LocateStatusType
-GIOP_C::IssueLocateRequest(const void   *objkey,
-			   const size_t  objkeysize)
+GIOP_C::IssueLocateRequest()
 {
   if (pd_state != GIOP_C::Idle)
     throw omniORB::fatalException(__FILE__,__LINE__,
       "GIOP_C::IssueLocateRequest() entered with the wrong state.");
 
-  clicksSet(StrandScavenger::clientCallTimeLimit());
-
-  CORBA::ULong msgsize = sizeof(MessageHeader::LocateRequest) + 
-                         sizeof(CORBA::ULong);
-  CORBA::ULong bodysize = omni::align_to(msgsize,omni::ALIGN_4) - msgsize;
-
-  bodysize += sizeof (CORBA::ULong) + sizeof(CORBA::ULong) + objkeysize;
-  msgsize += bodysize;
-
   pd_state = GIOP_C::RequestInProgress;
-  pd_request_id = newRequestID();
 
-  WrMessageSize(msgsize);
-  put_char_array((CORBA::Char*) MessageHeader::LocateRequest,
-		 sizeof(MessageHeader::LocateRequest),
-		 omni::ALIGN_1, 1, 1);
-  operator>>= ((CORBA::ULong)bodysize,*this);
+  // XXX Should rewite to use giopMarshaller
+  (void) pd_cdrStream->outputLocateMessageBegin(pd_ior);
 
-  operator>>= (pd_request_id,*this);
-
-  operator>>= ((CORBA::ULong) objkeysize,*this);
-  put_char_array((CORBA::Char*) objkey, objkeysize);
+  pd_cdrStream->outputMessageEnd();
 
   pd_state = GIOP_C::WaitingForReply;
-  flush(1);
 
-  do {
-    RdMessageSize(0,omni::myByteOrder);
+  GIOP::LocateStatusType rc;
 
-    MessageHeader::HeaderType hdr;
-    get_char_array((CORBA::Char*)hdr, sizeof(MessageHeader::HeaderType),
-		   omni::ALIGN_1, 1);
+  rc = pd_cdrStream->inputLocateReplyMessageBegin();
 
-    pd_state = GIOP_C::ReplyIsBeingProcessed;
+  pd_state = GIOP_C::ReplyIsBeingProcessed;
 
-    if (hdr[0] != MessageHeader::LocateReply[0] ||
-	hdr[1] != MessageHeader::LocateReply[1] ||
-	hdr[2] != MessageHeader::LocateReply[2] ||
-	hdr[3] != MessageHeader::LocateReply[3] ||
-	hdr[4] != MessageHeader::LocateReply[4] ||
-	hdr[5] != MessageHeader::LocateReply[5] ||
-	hdr[7] != MessageHeader::LocateReply[7])
-      {
-	// Wrong header
-	setStrandIsDying();
-	OMNIORB_THROW_CONNECTION_BROKEN(0,CORBA::COMPLETED_MAYBE);
-      }
-
-    CORBA::ULong msgsize;
-    msgsize <<= *this;
-    if (hdr[6] != omni::myByteOrder) {
-      msgsize =  ((((msgsize) & 0xff000000) >> 24) | 
-		  (((msgsize) & 0x00ff0000) >> 8)  | 
-		  (((msgsize) & 0x0000ff00) << 8)  | 
-		  (((msgsize) & 0x000000ff) << 24));
-    }
-
-    if (msgsize > MaxMessageSize()) {
-      // message size has exceeded the limit
-      setStrandIsDying();
-      OMNIORB_THROW_CONNECTION_BROKEN(0,CORBA::COMPLETED_MAYBE);
-    }
-
-    RdMessageSize(msgsize,hdr[6]);
-
-    CORBA::ULong req_id;
-    req_id <<= *this;
-    if (req_id != pd_request_id) {
-      // Not the expected reply, skip the entire message
-      skip(RdMessageUnRead(),1);
-      pd_state = GIOP_C::RequestInProgress;
-      continue;
-    }
-  } while (0);
-  
-  CORBA::ULong rc;
-  rc <<= *this;
 
   switch (rc) {
   case GIOP::UNKNOWN_OBJECT:
   case GIOP::OBJECT_HERE:
   case GIOP::OBJECT_FORWARD:
+  case GIOP::OBJECT_FORWARD_PERM:
+  case GIOP::LOC_NEEDS_ADDRESSING_MODE:
+    break;
+  case GIOP::LOC_SYSTEM_EXCEPTION:
+    UnMarshallSystemException();
     break;
   default:
     // Should never receive anything other that the above
     // Same treatment as wrong header
-    setStrandIsDying();
+    pd_cdrStream->setTerminalError();
     OMNIORB_THROW_CONNECTION_BROKEN(0,CORBA::COMPLETED_MAYBE);
   }
-  return (GIOP::LocateStatusType)rc;
+  return rc;
 }
 
 
@@ -477,17 +255,17 @@ GIOP_C::UnMarshallSystemException()
 
   // Real code begins here
   CORBA::ULong len;
-  len <<= *this;
+  len <<= *pd_cdrStream;
   if (len > omniORB_GIOP_Basetypes_SysExceptRepoID_maxIDLen)
     OMNIORB_THROW(UNKNOWN,0,CORBA::COMPLETED_MAYBE);
 
   CORBA::Char repoid[omniORB_GIOP_Basetypes_SysExceptRepoID_maxIDLen];
 
-  get_char_array(repoid, len);
+  pd_cdrStream->get_char_array(repoid, len);
   CORBA::ULong m;
   CORBA::ULong s;
-  m <<= *this;
-  s <<= *this;
+  m <<= *pd_cdrStream;
+  s <<= *pd_cdrStream;
   RequestCompleted();
   switch (s) {
   case CORBA::COMPLETED_YES:
