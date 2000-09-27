611c863d17b1b39849ed1ff1f80034b80141e0cc
sll <sll>
2000-09-27 17:51:58 +0000

    New constants defined in the GIOP and IIOP modules.

--- src/lib/omniORB/orbcore/constants.cc
+++ src/lib/omniORB/orbcore/constants.cc
@@ -29,6 +29,9 @@
 
 /*
   $Log$
+  Revision 1.15.2.2  2000/09/27 17:51:58  sll
+  New constants defined in the GIOP and IIOP modules.
+
   Revision 1.15.2.1  2000/07/17 10:35:51  sll
   Merged from omni3_develop the diff between omni3_0_0_pre3 and omni3_0_0.
 
@@ -87,80 +90,183 @@
 //
 */
 
-#include <omniORB3/CORBA.h>
-
-#ifdef HAS_pch
-#pragma hdrstop
-#endif
-
+#include <omniORB4/CORBA.h>
 
 // See the description of this variable in omniInternal.h
-const char* omniORB_3_0 = "omniORB version 3.0.x";
+const char* omniORB_4_0 = "omniORB version 4.0.x";
+
+const CORBA::ULong omniORB_TAG_ORB_TYPE = 0x41545400; // ATT\x00
+
+//////////////////////////////////////////////////////////////////////////
+//                GIOP AddressingDisposition  constants                 //
+//////////////////////////////////////////////////////////////////////////
+const GIOP::AddressingDisposition GIOP::KeyAddr = 0;
+const GIOP::AddressingDisposition GIOP::ProfileAddr = 1;
+const GIOP::AddressingDisposition GIOP::ReferenceAddr = 2;
 
 
-const IOP::ProfileId       IOP::TAG_INTERNET_IOP        = 0;
-const IOP::ProfileId       IOP::TAG_MULTIPLE_COMPONENTS = 1;
-const IOP::ServiceID       IOP::TransactionService      = 0;
-const CORBA::Char          IIOP::current_major          = 1;
-const CORBA::Char          IIOP::current_minor          = 0;
-const CORBA::UShort        IIOP::DEFAULT_CORBALOC_PORT  = 2809;
+//////////////////////////////////////////////////////////////////////////
+//                IOR profile tag constants                             //
+//////////////////////////////////////////////////////////////////////////
+const IOP::ProfileId  IOP::TAG_INTERNET_IOP = 0;
+const IOP::ProfileId  IOP::TAG_MULTIPLE_COMPONENTS = 1;
+const IOP::ProfileId  IOP::TAG_SCCP_IOP = 2;
+const CORBA::UShort   IIOP::DEFAULT_CORBALOC_PORT  = 2809;
+
+const IOP::ComponentId IOP::TAG_ORB_TYPE = 0;
+const IOP::ComponentId IOP::TAG_CODE_SETS = 1;
+const IOP::ComponentId IOP::TAG_POLICIES = 2;
+const IOP::ComponentId IOP::TAG_ALTERNATE_IIOP_ADDRESS = 3;
+const IOP::ComponentId IOP::TAG_COMPLETE_OBJECT_KEY = 5;
+const IOP::ComponentId IOP::TAG_ENDPOINT_ID_POSITION = 6;
+const IOP::ComponentId IOP::TAG_LOCATION_POLICY = 12;
+const IOP::ComponentId IOP::TAG_ASSOCIATION_OPTIONS = 13;
+const IOP::ComponentId IOP::TAG_SEC_NAME = 14;
+const IOP::ComponentId IOP::TAG_SPKM_1_SEC_MECH = 15;
+const IOP::ComponentId IOP::TAG_SPKM_2_SEC_MECH = 16;
+const IOP::ComponentId IOP::TAG_KERBEROSV5_SEC_MECH = 17;
+const IOP::ComponentId IOP::TAG_CSI_ECMA_SECRET_SEC_MECH = 18;
+const IOP::ComponentId IOP::TAG_CSI_ECMA_HYBRID_SEC_MECH = 19;
+const IOP::ComponentId IOP::TAG_SSL_SEC_TRANS = 20;
+const IOP::ComponentId IOP::TAG_CSI_ECMA_PUBLIC_SEC_MECH = 21;
+const IOP::ComponentId IOP::TAG_GENERIC_SEC_MECH = 22;
+const IOP::ComponentId IOP::TAG_FIREWALL_TRANS = 23;
+const IOP::ComponentId IOP::TAG_SCCP_CONTACT_INFO = 24;
+const IOP::ComponentId IOP::TAG_JAVA_CODEBASE = 25;
+const IOP::ComponentId IOP::TAG_DCE_STRING_BINDING = 100;
+const IOP::ComponentId IOP::TAG_DCE_BINDING_NAME = 101;
+const IOP::ComponentId IOP::TAG_DCE_NO_PIPES = 102;
+const IOP::ComponentId IOP::TAG_DCE_SEC_MECH = 103;
+const IOP::ComponentId IOP::TAG_INET_SEC_TRANS = 123;
+
+static struct {
+  IOP::ComponentId id;
+  const char*      name;
+} componentIDName[] = {
+  // Must arrange in ascending order of ComponentId.
+  { IOP::TAG_ORB_TYPE, "TAG_ORB_TYPE" },
+  { IOP::TAG_CODE_SETS, "TAG_CODE_SETS" },
+  { IOP::TAG_POLICIES, "TAG_POLICIES" },
+  { IOP::TAG_ALTERNATE_IIOP_ADDRESS, "TAG_ALTERNATE_IIOP_ADDRESS" },
+  { IOP::TAG_COMPLETE_OBJECT_KEY, "TAG_COMPLETE_OBJECT_KEY" },
+  { IOP::TAG_ENDPOINT_ID_POSITION, "TAG_ENDPOINT_ID_POSITION" },
+  { IOP::TAG_LOCATION_POLICY, "TAG_LOCATION_POLICY" },
+  { IOP::TAG_ASSOCIATION_OPTIONS, "TAG_ASSOCIATION_OPTIONS" },
+  { IOP::TAG_SEC_NAME, "TAG_SEC_NAME" },
+  { IOP::TAG_SPKM_1_SEC_MECH, "TAG_SPKM_1_SEC_MECH" },
+  { IOP::TAG_SPKM_2_SEC_MECH,  "TAG_SPKM_2_SEC_MECH" },
+  { IOP::TAG_KERBEROSV5_SEC_MECH, "TAG_KERBEROSV5_SEC_MECH" },
+  { IOP::TAG_CSI_ECMA_SECRET_SEC_MECH, "TAG_CSI_ECMA_SECRET_SEC_MECH" },
+  { IOP::TAG_CSI_ECMA_HYBRID_SEC_MECH, "TAG_CSI_ECMA_HYBRID_SEC_MECH" },
+  { IOP::TAG_SSL_SEC_TRANS, "TAG_SSL_SEC_TRANS" },
+  { IOP::TAG_CSI_ECMA_PUBLIC_SEC_MECH, "TAG_CSI_ECMA_PUBLIC_SEC_MECH" },
+  { IOP::TAG_GENERIC_SEC_MECH, "TAG_GENERIC_SEC_MECH" },
+  { IOP::TAG_FIREWALL_TRANS, "TAG_FIREWALL_TRANS" },
+  { IOP::TAG_SCCP_CONTACT_INFO, "TAG_SCCP_CONTACT_INFO" },
+  { IOP::TAG_JAVA_CODEBASE, "TAG_JAVA_CODEBASE" },
+  { IOP::TAG_DCE_STRING_BINDING, "TAG_DCE_STRING_BINDING" },
+  { IOP::TAG_DCE_BINDING_NAME, "TAG_DCE_BINDING_NAME" },
+  { IOP::TAG_DCE_NO_PIPES, "TAG_DCE_NO_PIPES" },
+  { IOP::TAG_DCE_SEC_MECH, "TAG_DCE_SEC_MECH" },
+  { IOP::TAG_INET_SEC_TRANS, "TAG_INET_SEC_TRANS" },
+  { 0, 0 }
+};
 
 
-const GIOP_Basetypes::MessageHeader::HeaderType
-         GIOP_Basetypes::MessageHeader::Request = {
-	   'G','I','O','P',
-	   1,0,
-	   _OMNIORB_HOST_BYTE_ORDER_,
-	   GIOP::Request
-	 };
+const char*
+IOP::ComponentIDtoName(IOP::ComponentId v)
+{
+  static int tablesize = 0;
 
-const GIOP_Basetypes::MessageHeader::HeaderType 
-         GIOP_Basetypes::MessageHeader::Reply = {
-	   'G','I','O','P',
-	   1,0,
-	   _OMNIORB_HOST_BYTE_ORDER_,
-	   GIOP::Reply
-	 };
+  if (!tablesize) {
+    while (componentIDName[tablesize].name) tablesize++;
+  }
 
-const GIOP_Basetypes::MessageHeader::HeaderType 
-         GIOP_Basetypes::MessageHeader::CancelRequest = {
-	   'G','I','O','P',
-	   1,0,
-	   _OMNIORB_HOST_BYTE_ORDER_,
-	   GIOP::CancelRequest
-	 };
+  int top = tablesize;
+  int bottom = 0;
 
-const GIOP_Basetypes::MessageHeader::HeaderType 
-         GIOP_Basetypes::MessageHeader::LocateRequest = {
-	   'G','I','O','P',
-	   1,0,
-	   _OMNIORB_HOST_BYTE_ORDER_,
-	   GIOP::LocateRequest
-	 };
+  do {
+    int index = (top + bottom) >> 1;
+    IOP::ComponentId id = componentIDName[index].id;
+    if (id == v) {
+      return componentIDName[index].name;
+    }
+    else if (id > v) {
+      top = index;
+    }
+    else {
+      bottom = index + 1;
+    }
+  } while (top != bottom);
 
-const GIOP_Basetypes::MessageHeader::HeaderType
-         GIOP_Basetypes::MessageHeader::LocateReply = {
-	   'G','I','O','P',
-	   1,0,
-	   _OMNIORB_HOST_BYTE_ORDER_,
-	   GIOP::LocateReply
-	 };
+  return 0;
+}
 
-const GIOP_Basetypes::MessageHeader::HeaderType 
-         GIOP_Basetypes::MessageHeader::CloseConnection= {
-	   'G','I','O','P',
-	   1,0,
-	   _OMNIORB_HOST_BYTE_ORDER_,
-	   GIOP::CloseConnection
-	 };
+const IOP::ServiceID IOP::TransactionService = 0;
+const IOP::ServiceID IOP::CodeSets = 1; 
+const IOP::ServiceID IOP::ChainBypassCheck = 2;
+const IOP::ServiceID IOP::ChainBypassInfo = 3;
+const IOP::ServiceID IOP::LogicalThreadId = 4;
+const IOP::ServiceID IOP::BI_DIR_IIOP = 5;
+const IOP::ServiceID IOP::SendingContextRunTime = 6;
+const IOP::ServiceID IOP::INVOCATION_POLICIES = 7;
+const IOP::ServiceID IOP::FORWARDED_IDENTITY = 8;
+const IOP::ServiceID IOP::UnknownExceptionInfo = 9;
+const IOP::ServiceID IOP::RTCorbaPriority = 10;
+const IOP::ServiceID IOP::RTCorbaPriorityRange = 11;
 
-const GIOP_Basetypes::MessageHeader::HeaderType 
-         GIOP_Basetypes::MessageHeader::MessageError = {
-	   'G','I','O','P',
-	   1,0,
-	   _OMNIORB_HOST_BYTE_ORDER_,
-	   GIOP::MessageError
-	 };
+static struct {
+  IOP::ServiceID id;
+  const char* name;
+} ServiceIDName[] = {
+  // Must arrange in ascending order of ServiceId.
+  { IOP::TransactionService, "TransactionService" }, 
+  { IOP::CodeSets, "CodeSets" }, 
+  { IOP::ChainBypassCheck, "ChainBypassCheck" },
+  { IOP::ChainBypassInfo, "ChainBypassInfo" },
+  { IOP::LogicalThreadId, "LogicalThreadId" },
+  { IOP::BI_DIR_IIOP, "BI_DIR_IIOP" }, 
+  { IOP::SendingContextRunTime, "SendingContextRunTime" },
+  { IOP::INVOCATION_POLICIES, "INVOCATION_POLICIES" },
+  { IOP::FORWARDED_IDENTITY, "FORWARDED_IDENTITY" },
+  { IOP::UnknownExceptionInfo, "UnknownExceptionInfo" },
+  { IOP::RTCorbaPriority, "RTCorbaPriority" },
+  { IOP::RTCorbaPriorityRange, "RTCorbaPriorityRange" },
+  { 0, 0 }
+};
+
+const char*
+IOP::ServiceIDtoName(IOP::ServiceID v)
+{
+  static int tablesize = 0;
+
+  if (!tablesize) {
+    while (ServiceIDName[tablesize].name) tablesize++;
+  }
+
+  int top = tablesize;
+  int bottom = 0;
+
+  do {
+    int index = (top + bottom) >> 1;
+    IOP::ServiceID id = ServiceIDName[index].id;
+    if (id == v) {
+      return ServiceIDName[index].name;
+    }
+    else if (id > v) {
+      top = index;
+    }
+    else {
+      bottom = index + 1;
+    }
+  } while (top != bottom);
+
+  return 0;
+}
+
+///////////////////////////////////////////////////////////////////////////
+//               Repository IDs                                          //
+///////////////////////////////////////////////////////////////////////////
 
 // Length of the longest system exception IR repository ID
 //   it is currently "IDL:omg.org/CORBA/TRANSACTION_ROLLEDBACK:1.0"
@@ -296,6 +402,7 @@ const GIOP_Basetypes::_SysExceptRepoID
 const GIOP_Basetypes::_SysExceptRepoID 
       GIOP_Basetypes::SysExceptRepoID::WRONG_TRANSACTION ((CORBA::Char *)
 				  "IDL:omg.org/CORBA/WRONG_TRANSACTION");
+
 
 //////////////////////////////////////////////////////////////////////
 // Each pseudo object type must be assigned a magic number.
