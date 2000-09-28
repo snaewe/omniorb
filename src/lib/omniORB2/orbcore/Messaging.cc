475d6be7974006affb3e2faf00d5e3364f9e0720
djs <djs>
2000-09-28 23:18:26 +0000

    Removed a spurious closing curly bracket

--- src/lib/omniORB2/orbcore/Messaging.cc
+++ src/lib/omniORB2/orbcore/Messaging.cc
@@ -29,6 +29,9 @@
 /*
  $Id$
  $Log$
+ Revision 1.1.2.5  2000/09/28 23:18:26  djs
+ Removed a spurious closing curly bracket
+
  Revision 1.1.2.4  2000/09/28 18:29:39  djs
  Bugfixes in Poller (wrt timout behaviour and is_ready function)
  Removed traces of Private POA/ internal ReplyHandler servant for Poller
@@ -130,34 +133,32 @@ void Messaging::Poller::associated_handler(Messaging::ReplyHandler_ptr handler){
 // Returns TRUE iff the results are ready to be consumed.
 // timeout is in seconds, ULONG_MAX means forever
 CORBA::Boolean Messaging::Poller::is_ready(CORBA::ULong timeout){
-    omni_mutex_lock lock(pd_state_lock);
-    // It's not clear from the spec what to do if the value has already
-    // been consumed. FALSE would probably mean try again later, which
-    // would never work. Therefore return TRUE, causing the client to
-    // attempt to acquire the data and OBJECT_NOT_EXIST being thrown.
-    if (reply_already_read) return 1;
-
-    // Note that just because the data is available doesn't mean this
-    // thread will be able to get it.
-    if (reply_received) return 1;
-
-    // Reply must not have been received yet.
-    if (timeout == 0) return 0;
-
-    if (timeout == ULONG_MAX){
-      // block indefinitely until I can return TRUE
-      pd_state_cond.wait();
-    }else{
-      // block with a timeout (timeout seconds)
-      unsigned long sec, nsec;
-      omni_thread::get_time(&sec, &nsec, 0, timeout*1000);
-      pd_state_cond.timedwait(sec, nsec);
-    }
-    // Again remember that just because the data has arrived, doesn't
-    // give this thread any right to it (First-Come-First-Served)
-    return reply_received;
+  omni_mutex_lock lock(pd_state_lock);
+  // It's not clear from the spec what to do if the value has already
+  // been consumed. FALSE would probably mean try again later, which
+  // would never work. Therefore return TRUE, causing the client to
+  // attempt to acquire the data and OBJECT_NOT_EXIST being thrown.
+  if (reply_already_read) return 1;
+  
+  // Note that just because the data is available doesn't mean this
+  // thread will be able to get it.
+  if (reply_received) return 1;
+  
+  // Reply must not have been received yet.
+  if (timeout == 0) return 0;
+  
+  if (timeout == ULONG_MAX){
+    // block indefinitely until I can return TRUE
+    pd_state_cond.wait();
+  }else{
+    // block with a timeout (timeout seconds)
+    unsigned long sec, nsec;
+    omni_thread::get_time(&sec, &nsec, 0, timeout*1000);
+    pd_state_cond.timedwait(sec, nsec);
   }
-
+  // Again remember that just because the data has arrived, doesn't
+  // give this thread any right to it (First-Come-First-Served)
+  return reply_received;
 }
 
 CORBA::Boolean Messaging::Poller::is_from_poller(){
