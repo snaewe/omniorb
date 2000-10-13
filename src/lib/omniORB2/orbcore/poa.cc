87ecd1f74de3de86be0b159af02fffd208ae1e1e
djr <djr>
2000-10-13 09:26:08 +0000

    Fixed race between object-deactivation and completion of last invocation.

--- src/lib/omniORB2/orbcore/poa.cc
+++ src/lib/omniORB2/orbcore/poa.cc
@@ -29,6 +29,9 @@
 
 /*
   $Log$
+  Revision 1.1.2.22  2000/10/13 09:26:09  djr
+  Fixed race between object-deactivation and completion of last invocation.
+
   Revision 1.1.2.21  2000/06/22 10:40:16  dpg1
   exception.h renamed to exceptiondefs.h to avoid name clash on some
   platforms.
@@ -1411,10 +1414,15 @@ omniOrbPOA::lastInvocationHasCompleted(omniLocalIdentity* id)
 
   PortableServer::ServantActivator_ptr sa = 0;
 
+  // This lock _could_ go inside the body of the 'if' below, but I want
+  // to ensure that we take this lock no matter what.  The reason is to
+  // ensure that detached_object() is called (in deactivate_object())
+  // before met_detached_object() (below).  Otherwise we get a nasty
+  // race ...
+  pd_lock.lock();
+
   if( (pd_policy.req_processing == RPP_SERVANT_MANAGER &&
        pd_policy.retain_servants) || pd_dying ) {
-
-    pd_lock.lock();
 
     // The omniLocalIdentity still holds a reference to us, and
     // we hold a reference to the servant activator, so we don't
@@ -1427,9 +1435,9 @@ omniOrbPOA::lastInvocationHasCompleted(omniLocalIdentity* id)
       // Wait for apparent destruction.
       while( !pd_destroyed )  pd_deathSignal.wait();
     }
-
-    pd_lock.unlock();
   }
+
+  pd_lock.unlock();
 
   PortableServer::Servant servant = DOWNCAST(id->servant());
 
