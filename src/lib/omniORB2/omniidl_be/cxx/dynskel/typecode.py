e58acd237e1e9e8023d918ca9ad04d1fbc533939
sll <sll>
2001-01-24 13:47:58 +0000

    omniidl cxx backend bug fix.

--- src/lib/omniORB2/omniidl_be/cxx/dynskel/typecode.py
+++ src/lib/omniORB2/omniidl_be/cxx/dynskel/typecode.py
@@ -28,6 +28,9 @@
 
 # $Id$
 # $Log$
+# Revision 1.14.2.11  2001/01/24 13:47:58  sll
+# omniidl cxx backend bug fix.
+#
 # Revision 1.14.2.10  2000/08/18 09:42:44  djs
 # Fixed silly typo in last fix (fix == s/definied/defined/)
 #
@@ -536,6 +539,8 @@ def visitUnion(node):
     oldbottomhalf = self.bottomhalf
     self.bottomhalf = util.StringStream()
 
+    insideModule = self.__immediatelyInsideModule
+    self.__immediatelyInsideModule = 0
     
     # need to build a static array of node members in a similar fashion
     # to structs
@@ -618,6 +623,8 @@ static CORBA::TypeCode_ptr @mangled_name@ = CORBA::TypeCode::PR_union_tc("@repoI
     
     defineName(unionmember_mangled_name)
     defineName(mangled_name)
+
+    self.__immediatelyInsideModule = insideModule
     
     external_linkage(node)
 
