2f8e678b28a4e91c8ca5c4bf37af2ea856fc1392
dpg1 <dpg1>
2001-04-23 13:55:31 +0000

    Bug in sort_exceptions().

--- src/lib/omniORB2/omniidl_be/cxx/skutil.py
+++ src/lib/omniORB2/omniidl_be/cxx/skutil.py
@@ -28,6 +28,9 @@
 
 # $Id$
 # $Log$
+# Revision 1.15.2.7  2001/04/23 13:55:32  dpg1
+# Bug in sort_exceptions().
+#
 # Revision 1.15.2.6  2000/08/23 15:46:27  djs
 # MSVC5/6 workaround when marshalling a return value which is a
 # multidimensional array of basic things.
@@ -599,13 +602,9 @@ def unmarshal_string_via_temporary(variable_name, stream_name):
 def sort_exceptions(ex):
     # sort the exceptions into lexicographical order
     def lexicographic(exception_a, exception_b):
-        # use their full C++ name
-        name_a = string.join(id.mapID(exception_a.scopedName()))
-        name_b = string.join(id.mapID(exception_b.scopedName()))
-        # name_a <=> name_b
-        if name_a < name_b: return -1
-        if name_a > name_b: return 1
-        return 0
+        name_a = exception_a.repoId()
+        name_b = exception_b.repoId()
+        return cmp(name_a, name_b)
         
     raises = ex[:]
     raises.sort(lexicographic)
