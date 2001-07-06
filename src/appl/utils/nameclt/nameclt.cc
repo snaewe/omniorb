6f26bed29b900a32dbbb4fc6bbb970ff09168c17
dpg1 <dpg1>
2001-07-06 14:24:43 +0000

    nameclt did not cope with a nil BindingIterator.

--- src/appl/utils/nameclt/nameclt.cc
+++ src/appl/utils/nameclt/nameclt.cc
@@ -209,6 +209,9 @@ main(int argc, char **argv)
 
       context->list(0, bl, bi);
 
+      if (CORBA::is_nil(bi))
+	goto done;
+
       while (bi->next_one(b)) {
 
 	CORBA::String_var sname = omniURI::nameToString(b->binding_name);
