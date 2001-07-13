e5693e5eb56fd2b2294212aebb671df5933ef4fc
sll <sll>
2001-07-13 15:37:21 +0000

    Preliminary changes for windows NT.

--- src/lib/omniORB/orbcore/ssl/dir.mk
+++ src/lib/omniORB/orbcore/ssl/dir.mk
@@ -15,12 +15,19 @@ DIR_CPPFLAGS += $(OPEN_SSL_CPPFLAGS)
 SSL_OBJS      = $(SSL_SRCS:.cc=.o)
 CXXSRCS       = $(SSL_SRCS)
 
+ifdef Win32Platform
+EXTRA_LIBS    = wsock32.lib advapi32.lib
+SHARED_ONLY_OBJS = msvcdllstub.o
+endif
+
 LIB_NAME     := omnisslTP
 LIB_VERSION  := $(OMNIORB_VERSION)
 LIB_OBJS     := $(SSL_OBJS)
 LIB_IMPORTS  := $(patsubst %,$(LibPathPattern),../shared) \
                 $(patsubst %,$(LibSearchPattern),omniORB$(OMNIORB_MAJOR_VERSION)) \
+                $(OMNIASYNCINVOKER_LIB) \
                 $(OMNITHREAD_LIB) # $(OPEN_SSL_LIB)
+LIB_SHARED_ONLY_OBJS := $(SHARED_ONLY_OBJS)
 
 
 include $(BASE_OMNI_TREE)/mk/mklib.mk
