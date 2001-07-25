bccc4fb4d05bcd3e3e553d56c7db15b220ed9145
dpg1 <dpg1>
2001-07-25 10:52:19 +0000

    Hack the showast.py back-end so it is clear that it's broken.

--- src/lib/omniORB2/omniidl_be/dir.mk
+++ src/lib/omniORB2/omniidl_be/dir.mk
@@ -19,5 +19,5 @@ export::
 export:: __init__.py
 	@(file="__init__.py"; dir="$(PYLIBDIR)"; $(ExportFileToDir))
 
-export:: showast.py
-	@(file="showast.py"; dir="$(PYLIBDIR)"; $(ExportFileToDir))
+# export:: showast.py
+# 	@(file="showast.py"; dir="$(PYLIBDIR)"; $(ExportFileToDir))
