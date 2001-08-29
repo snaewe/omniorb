9bcc0989bf4746133bf12df7d6f071febcfd9e00
sll <sll>
2001-08-29 17:53:05 +0000

    Replaced gatekeeper with serverTransportRule to determine whether a connection should be accepted.

--- src/lib/omniORB/orbcore/giopWorker.cc
+++ src/lib/omniORB/orbcore/giopWorker.cc
@@ -29,6 +29,10 @@
 
 /*
   $Log$
+  Revision 1.1.4.4  2001/08/29 17:53:05  sll
+  Replaced gatekeeper with serverTransportRule to determine whether a
+  connection should be accepted.
+
   Revision 1.1.4.3  2001/07/13 15:29:30  sll
   Worker now always calls notifyWkDone when it has finish one task.
 
@@ -44,13 +48,13 @@
 */
 
 #include <omniORB4/CORBA.h>
-#include <gatekeeper.h>
 #include <invoker.h>
 #include <giopServer.h>
 #include <giopWorker.h>
 #include <giopStrand.h>
 #include <giopStream.h>
 #include <GIOP_S.h>
+#include <transportRules.h>
 
 OMNI_NAMESPACE_BEGIN(omni)
 
@@ -69,14 +73,86 @@ giopWorker::execute() {
 
   CORBA::Boolean exit_on_error;
 
-  if (!pd_strand->gatekeeper_checked && !gateKeeper::checkConnect(pd_strand)) {
+  if (!pd_strand->gatekeeper_checked) {
+
+    transportRules::sequenceString actions;
+    CORBA::ULong matchedRule;
+
+    CORBA::Boolean acceptconnection;
+    CORBA::Boolean dumprule = 0;
+    const char* why;
+
+    if ( !(acceptconnection = transportRules::serverRules().
+                              match(pd_strand->connection->peeraddress(),
+				    actions,matchedRule)) ) {
+
+      why = (const char*) "no matching rule is found";
+    }
+
+    if ( acceptconnection ) {
+      acceptconnection = 0;
+
+      CORBA::ULong i;
+      const char* transport = strchr(pd_strand->connection->peeraddress(),':');
+      OMNIORB_ASSERT(transport);
+      transport++;
+      i = 0;
+      for (i = 0; i < actions.length(); i++ ) {
+	size_t len = strlen(actions[i]);
+	if (strncmp(actions[i],transport,len) == 0 ) {
+	  acceptconnection = 1;
+	  break;
+	}
+	else if ( strcmp(actions[i],"none") == 0 ) {
+	  why = (const char*) "no connection is granted by this rule: ";
+	  dumprule = 1;
+	  break;
+	}
+      }
+      if ( i == actions.length() ) {
+	why = (const char*) "the transport type is not in this rule: ";
+	dumprule = 1;
+      }
+    }
+
+    if ( !acceptconnection ) {
+      if ( omniORB::trace(1) ) {
+	omniORB::logger log;
+	log << "Connection from " << pd_strand->connection->peeraddress()
+	    << " is rejected because " << why;
+	if (dumprule) {
+	  CORBA::String_var rule;
+	  rule = transportRules::serverRules().dumpRule(matchedRule);
+	  log << "\"" << (const char*) rule << "\"";
+	}
+	log << "\n";
+      }
+      {
+	ASSERT_OMNI_TRACEDMUTEX_HELD(*omniTransportLock,0);
+	omni_tracedmutex_lock sync(*omniTransportLock);
+	pd_strand->safeDelete();
+      }
       pd_server->notifyWkDone(this,1);
       return;
-  }
-  else {
+    }
+
     pd_strand->gatekeeper_checked = 1;
+
+
+    if ( omniORB::trace(5) ) {
+
+      CORBA::String_var rule;
+      rule = transportRules::serverRules().dumpRule(matchedRule);
+
+      omniORB::logger log;
+      log << "Accepted connection from " 
+	  << pd_strand->connection->peeraddress()
+	  << " because of this rule: \""
+	  << (const char*) rule << "\"\n";
+    }
   }
 
+  
   CORBA::Boolean done = pd_singleshot;
 
   do {
