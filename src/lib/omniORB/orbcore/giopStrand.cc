d2167393cb37243a59d95c0149d12ecabfead39a
sll <sll>
2001-08-29 17:52:34 +0000

    Make sure that the invariant of the dtor is satisfied in safeDelete.

--- src/lib/omniORB/orbcore/giopStrand.cc
+++ src/lib/omniORB/orbcore/giopStrand.cc
@@ -28,6 +28,9 @@
 
 /*
   $Log$
+  Revision 1.1.4.9  2001/08/29 17:52:34  sll
+  Make sure that the invariant of the dtor is satisfied in safeDelete.
+
   Revision 1.1.4.8  2001/08/24 15:21:13  sll
   Corrected a bug in the conversion from {in,out}ConScanPeriod to idleclicks.
 
@@ -284,6 +287,7 @@ giopStrand::deleteStrandAndConnection(CORBA::Boolean forced)
 	<< (isClient() ? " to " : " from ")
 	<< (const char*)peeraddr << "\n";
   }
+  pd_state = DYING;     // satisfy the invariant in the dtor.
   delete this;
 }
 
