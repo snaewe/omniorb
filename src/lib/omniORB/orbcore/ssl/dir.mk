f062323096e37c1d360be7abdca9fde8bec7863a
sll <sll>
2001-09-13 15:22:45 +0000

    Correct rules for win32.

--- src/lib/omniORB/orbcore/ssl/dir.mk
+++ src/lib/omniORB/orbcore/ssl/dir.mk
@@ -17,7 +17,7 @@ SSL_OBJS      = $(SSL_SRCS:.cc=.o)
 CXXSRCS       = $(SSL_SRCS)
 
 ifdef Win32Platform
-EXTRA_LIBS    = wsock32.lib advapi32.lib
+EXTRA_LIBS    = $(SOCKET_LIB) advapi32.lib
 SHARED_ONLY_OBJS = msvcdllstub.o
 endif
 
@@ -25,9 +25,9 @@ LIB_NAME     := omnisslTP
 LIB_VERSION  := $(OMNIORB_VERSION)
 LIB_OBJS     := $(SSL_OBJS)
 LIB_IMPORTS  := $(patsubst %,$(LibPathPattern),../shared) \
-                $(patsubst %,$(LibSearchPattern),omniORB$(OMNIORB_MAJOR_VERSION)) \
+                $(OMNIORB_DLL_NAME) \
                 $(OMNIASYNCINVOKER_LIB) \
-                $(OMNITHREAD_LIB) # $(OPEN_SSL_LIB)
+                $(OMNITHREAD_LIB) $(EXTRA_LIBS) $(OPEN_SSL_LIB)
 LIB_SHARED_ONLY_OBJS := $(SHARED_ONLY_OBJS)
 
 
