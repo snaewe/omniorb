65f81280c609f2892d36750da4a9018df2f27b94
dgrisby <dgrisby>
2002-08-16 15:58:20 +0000

    Makefile tweaks to allow all COS interfaces to be built.

--- src/services/mklib/libdefs.mk
+++ src/services/mklib/libdefs.mk
@@ -5,15 +5,15 @@
 # Or just the Notification interfaces:
 COS_INTERFACES = $(TIMEBASE_INT) $(COS_EVT_INT) $(COS_NFY_INT) $(COS_LCB_INT)
 # Or pick your own subset
-# COS_INTERFACES = <pick you own subset from below>
+# COS_INTERFACES = <pick you own subset from cosinterfaces.mk>
 
 
-DIR_IDLFLAGS  += -Wbuse_quotes
+DIR_IDLFLAGS  += -Wbuse_quotes -I$(BASE_OMNI_TREE)/idl
 
 # omniORB now properly supports long long, so we don't need to turn it off...
 #DIR_IDLFLAGS  += -DNOLONGLONG
 
-DIR_CPPFLAGS = $(CORBA_CPPFLAGS) -D_COS_LIBRARY
+DIR_CPPFLAGS = $(CORBA_CPPFLAGS) -D_COS_LIBRARY -DENABLE_CLIENT_IR_SUPPORT
 
 %.DynSK.cc %SK.cc: %.idl
 	$(CORBA_IDL) $(DIR_IDLFLAGS) $^	
