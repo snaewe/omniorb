79ec2aff1cba6b95dc90a29712249a2f6073ac82
dgrisby <dgrisby>
2002-10-30 16:48:50 +0000

    Log flush broken when using log function.

--- src/lib/omniORB/orbcore/logIOstream.cc
+++ src/lib/omniORB/orbcore/logIOstream.cc
@@ -28,6 +28,9 @@
  
 /*
   $Log$
+  Revision 1.8.2.10  2002/10/30 16:49:03  dgrisby
+  Log flush broken when using log function.
+
   Revision 1.8.2.9  2002/01/15 16:38:13  dpg1
   On the road to autoconf. Dependencies refactored, configure.ac
   written. No makefiles yet.
@@ -345,9 +348,12 @@ omniORB::logger::operator<<(const CORBA::SystemException& ex)
 void
 omniORB::logger::flush()
 {
-  if( (size_t)(pd_p - pd_buf) != strlen(pd_prefix) )
-    fprintf(stderr, "%s", pd_buf);
-
+  if( (size_t)(pd_p - pd_buf) != strlen(pd_prefix) ) {
+    if (logfunc())
+      logfunc()(pd_buf);
+    else
+      fprintf(stderr, "%s", pd_buf);
+  }
   pd_p = pd_buf + strlen(pd_prefix);
   *pd_p = '\0';
 }
