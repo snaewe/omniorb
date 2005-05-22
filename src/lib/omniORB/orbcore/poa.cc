bb1524cbfe88064bef88a904184089d2bb770672
dgrisby <dgrisby>
2005-05-22 12:32:23 +0000

    Race condition in POA destroy.

--- src/lib/omniORB/orbcore/poa.cc
+++ src/lib/omniORB/orbcore/poa.cc
@@ -29,6 +29,9 @@
 
 /*
   $Log$
+  Revision 1.2.2.44  2005/05/22 12:32:23  dgrisby
+  Race condition in POA destroy.
+
   Revision 1.2.2.43  2005/04/08 00:06:13  dgrisby
   Remove all remaining uses of logf.
 
@@ -2116,18 +2119,33 @@ omniOrbPOA::do_destroy(CORBA::Boolean etherealize_objects)
   ASSERT_OMNI_TRACEDMUTEX_HELD(poa_lock, 0);
   OMNIORB_ASSERT(pd_dying);
 
-  while( pd_children.length() ) {
-    try {
-      pd_children[0]->destroy(etherealize_objects, 1);
+  PortableServer::POA_var child;
+  while (1) {
+    {
+      omni_tracedmutex_lock sync(poa_lock);
+      if (pd_children.length())
+	child = PortableServer::POA::_duplicate(pd_children[0]);
+      else
+	child = PortableServer::POA::_nil();
     }
-    catch(CORBA::OBJECT_NOT_EXIST& ex) {
-      // Race with another thread destroying a child POA.
-      omni_thread::sleep(0, 100000000);
+    if (!CORBA::is_nil(child)) {
+      try {
+	child->destroy(etherealize_objects, 1);
+      }
+      catch(CORBA::OBJECT_NOT_EXIST& ex) {
+	// Race with another thread destroying a child POA.
+	omni_thread::sleep(0, 100000000);
+      }
+      catch (...) {
+	omniORB::logs(2, "Unexpected exception in do_destroy.");
+      }
     }
-    catch (...) {
-      omniORB::logs(2, "Unexpected exception in do_destroy.");
+    else {
+      // No more children
+      break;
     }
   }
+  child = PortableServer::POA::_nil();
 
   OMNIORB_ASSERT(pd_children.length() == 0);
 
