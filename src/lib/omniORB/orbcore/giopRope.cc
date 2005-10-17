64739911bf247ffe2a02f312a39e391f60b8dbc9
dgrisby <dgrisby>
2005-10-17 15:50:55 +0000

    Permit clients to scavenge bidir connections if no object references have been transmitted.

--- src/lib/omniORB/orbcore/giopRope.cc
+++ src/lib/omniORB/orbcore/giopRope.cc
@@ -28,6 +28,10 @@
 
 /*
   $Log$
+  Revision 1.1.4.28  2005/10/17 15:50:55  dgrisby
+  Permit clients to scavenge bidir connections if no object references
+  have been transmitted.
+
   Revision 1.1.4.27  2005/08/23 11:46:05  dgrisby
   Race condition in strand selection could lead to an assertion failure.
 
@@ -461,7 +465,7 @@ giopRope::releaseClient(IOP_C* iop_c) {
     giop_c->giopStreamList::insert(s->clients);
     // The strand is definitely idle from this point onwards, we
     // reset the idle counter so that it will be retired at the right time.
-    if ( s->isClient() && !s->biDir ) 
+    if ( s->isClient() && !s->biDir_has_callbacks ) 
       s->startIdleCounter();
   }
 
