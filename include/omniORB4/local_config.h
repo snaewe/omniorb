280377f6082cc15c46032f73392793741cdfd943
dgrisby <dgrisby>
2006-05-25 18:16:50 +0000

    Standard stream operators for String_var, Fixed, Exception, etc.

--- include/omniORB4/local_config.h
+++ include/omniORB4/local_config.h
@@ -7,11 +7,15 @@
 #define __LOCAL_CONFIG_H__
 
 // If you do not want to map CORBA::WChar to C++ wchar_t and instead want
-// it to map to a 16-bit entity, uncomment the following:
+// it to map to a 16-bit entity, uncomment the following two lines:
 
 // #define _CORBA_WCHAR_DECL unsigned short
 // #define SIZEOF_WCHAR      2
 
 
+// If you want operators for wide strings in wide streams, uncomment
+// the following:
+
+// #define OMNI_WIDE_STREAM_OPERATORS
 
 #endif
