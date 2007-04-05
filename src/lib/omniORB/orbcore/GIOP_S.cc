7025360f012f2af12e1877300bd2f098829e7318
dgrisby <dgrisby>
2007-04-05 15:05:24 +0000

    Finished call could be selected by a CancelRequest, leading to an infinite loop failing to handle it.

--- src/lib/omniORB/orbcore/GIOP_S.cc
+++ src/lib/omniORB/orbcore/GIOP_S.cc
@@ -29,6 +29,10 @@
 
 /*
   $Log$
+  Revision 1.1.6.10  2007/04/05 15:05:24  dgrisby
+  Finished call could be selected by a CancelRequest, leading to an
+  infinite loop failing to handle it.
+
   Revision 1.1.6.9  2006/11/09 15:36:17  dgrisby
   Code formatting fix.
 
@@ -236,6 +240,7 @@ GIOP_S::dispatcher() {
 
     pd_state = WaitForRequestHeader;
     calldescriptor(0);
+    requestId(0xffffffff);
 
     impl()->inputMessageBegin(this,impl()->unmarshalWildCardRequestHeader);
 
@@ -245,10 +250,7 @@ GIOP_S::dispatcher() {
       if (!pd_strand->stopIdleCounter()) {
 	// This strand has been expired by the scavenger. Don't
 	// process this call.
-	if (omniORB::trace(1)) {
-	  omniORB::logger l;
-	  l << "dispatcher cannot stop idle counter.\n";
-	}
+	omniORB::logs(1, "dispatcher cannot stop idle counter.\n");
 	pd_strand->state(giopStrand::DYING);
 	return 0;
       }
